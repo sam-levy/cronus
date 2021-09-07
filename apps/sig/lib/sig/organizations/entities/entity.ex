ef64210d705e2244f8d49b681ba72a0125c16e60
Samuel Levy <samulevy@gmail.com>
2021-09-07 00:18:36 -0300

    Cast assoc individual from entity and add entity_test

--- apps/sig/lib/sig/organizations/entities/entity.ex
+++ apps/sig/lib/sig/organizations/entities/entity.ex
@@ -1,7 +1,17 @@
 defmodule Sig.Organizations.Entities.Entity do
   use Sig.Schema
 
+  alias Sig.Organizations.Entities.Individual
+
   schema "entities" do
+    has_one :individual, Individual, on_replace: :update
+
     timestamps()
+  end
+
+  def new_individual_changeset(attrs) do
+    %__MODULE__{}
+    |> cast(attrs, [])
+    |> cast_assoc(:individual, with: &Individual.new_changeset/2, required: true)
   end
 end
---
+++ apps/sig/test/sig/organizations/entities/entity_test.exs
@@ -0,0 +1,40 @@
+defmodule Sig.Organizations.Entities.EntityTest do
+  use Sig.DataCase
+
+  alias Sig.Organizations.Entities.Entity
+  alias Sig.Organizations.Entities.Individual.Gender
+
+  describe "new_individual_changeset/1" do
+    test "valid params" do
+      individual_params = %{
+        name: Faker.Person.name(),
+        cpf: BrazilianDocuments.generate_cpf(),
+        gender: random_enum_value(Gender)
+      }
+
+      entity_params = %{
+        individual: individual_params
+      }
+
+      assert changeset = Entity.new_individual_changeset(entity_params)
+
+      assert changeset.valid?
+
+      assert changeset.changes.individual.changes == %{
+               name: individual_params[:name],
+               cpf: individual_params[:cpf],
+               gender: String.to_atom(individual_params[:gender])
+             }
+    end
+
+    test "missing required params" do
+      assert changeset = Entity.new_individual_changeset(%{})
+
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{
+               individual: ["can't be blank"],
+             }
+    end
+  end
+end
