bc579f5190ff50900b21c8731050f906f92698e9
Samuel Levy <samulevy@gmail.com>
2021-09-08 22:22:20 -0300

    Add companies assoc changesets to Entities

--- apps/sig/lib/sig/organizations/entities/entity.ex
+++ apps/sig/lib/sig/organizations/entities/entity.ex
@@ -1,10 +1,11 @@
 defmodule Sig.Organizations.Entities.Entity do
   use Sig.Schema
 
-  alias Sig.Organizations.Entities.Individual
+  alias Sig.Organizations.Entities.{Company, Individual}
 
   schema "entities" do
     has_one :individual, Individual, on_replace: :update
+    has_one :company, Company, on_replace: :update
 
     timestamps()
   end
@@ -13,5 +14,17 @@ defmodule Sig.Organizations.Entities.Entity do
     %__MODULE__{}
     |> cast(attrs, [])
     |> cast_assoc(:individual, with: &Individual.new_changeset/2, required: true)
+  end
+
+  def new_real_company_changeset(attrs) do
+    %__MODULE__{}
+    |> cast(attrs, [])
+    |> cast_assoc(:company, with: &Company.new_real_changeset/2, required: true)
+  end
+
+  def new_virtual_company_changeset(attrs) do
+    %__MODULE__{}
+    |> cast(attrs, [])
+    |> cast_assoc(:company, with: &Company.new_virtual_changeset/2, required: true)
   end
 end
--- apps/sig/test/sig/organizations/entities/entity_test.exs
+++ apps/sig/test/sig/organizations/entities/entity_test.exs
@@ -45,4 +45,65 @@ defmodule Sig.Organizations.Entities.EntityTest do
              }
     end
   end
+
+  describe "new_real_company_changeset/1" do
+    test "valid params" do
+      organzation_id = UUID.generate()
+
+      company_params = %{
+        trade_name: Faker.Company.name(),
+        registration_name: Faker.Company.name(),
+        cnpj: BrazilianDocuments.generate_cnpj(),
+        organization_id: organzation_id
+      }
+
+      entity_params = %{
+        company: company_params
+      }
+
+      assert changeset = Entity.new_real_company_changeset(entity_params)
+
+      assert changeset.valid?
+
+      assert %{
+               company: company_changeset
+             } = changeset.changes
+
+      assert company_changeset.changes == %{
+               trade_name: company_params[:trade_name],
+               registration_name: company_params[:registration_name],
+               cnpj: company_params[:cnpj],
+               organization_id: organzation_id
+             }
+    end
+  end
+
+  describe "new_virtual_company_changeset/1" do
+    test "valid params" do
+      organzation_id = UUID.generate()
+
+      company_params = %{
+        trade_name: Faker.Company.name(),
+        organization_id: organzation_id
+      }
+
+      entity_params = %{
+        company: company_params
+      }
+
+      assert changeset = Entity.new_virtual_company_changeset(entity_params)
+
+      assert changeset.valid?
+
+      assert %{
+               company: company_changeset
+             } = changeset.changes
+
+      assert company_changeset.changes == %{
+               trade_name: company_params[:trade_name],
+               organization_id: organzation_id,
+               is_virtual: true
+             }
+    end
+  end
 end
