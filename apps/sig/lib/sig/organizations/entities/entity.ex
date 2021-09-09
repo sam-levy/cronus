679903815120c3d5d558dd5a17949865f5859053
Samuel Levy <samulevy@gmail.com>
2021-09-09 09:02:00 -0300

    Remove Entity changesets

--- apps/sig/lib/sig/organizations/entities/entity.ex
+++ apps/sig/lib/sig/organizations/entities/entity.ex
@@ -9,22 +9,4 @@ defmodule Sig.Organizations.Entities.Entity do
 
     timestamps()
   end
-
-  def new_individual_changeset(attrs) do
-    %__MODULE__{}
-    |> cast(attrs, [])
-    |> cast_assoc(:individual, with: &Individual.new_changeset/2, required: true)
-  end
-
-  def new_real_company_changeset(attrs) do
-    %__MODULE__{}
-    |> cast(attrs, [])
-    |> cast_assoc(:company, with: &Company.new_real_changeset/2, required: true)
-  end
-
-  def new_virtual_company_changeset(attrs) do
-    %__MODULE__{}
-    |> cast(attrs, [])
-    |> cast_assoc(:company, with: &Company.new_virtual_changeset/2, required: true)
-  end
 end
