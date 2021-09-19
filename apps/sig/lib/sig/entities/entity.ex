198bfe4ce3ce8aa8838528c488ed9d71dfda4820
Samuel Levy <samulevy@gmail.com>
2021-09-19 18:53:01 -0300

    Remove unused associations from Entity

--- apps/sig/lib/sig/entities/entity.ex
+++ apps/sig/lib/sig/entities/entity.ex
@@ -1,15 +1,10 @@
 defmodule Sig.Entities.Entity do
   use Sig.Schema
 
-  alias Sig.Entities.Companies.Company
-  alias Sig.Entities.Individuals.Individual
   alias Sig.Organizations.Org
 
   schema "entities" do
     belongs_to :org, Org, primary_key: true
-
-    has_one :individual, Individual, on_replace: :update
-    has_one :company, Company, on_replace: :update
 
     timestamps()
   end
