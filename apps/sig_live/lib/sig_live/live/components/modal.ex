90eefc7994aad805c1a078f7962a366c54b78b09
Samuel Levy <samulevy@gmail.com>
2021-09-20 22:50:51 -0300

    Improve modal

--- apps/sig_live/lib/sig_live/live/components/modal.ex
+++ apps/sig_live/lib/sig_live/live/components/modal.ex
@@ -10,18 +10,18 @@ defmodule SigLive.Components.Modal do
   def render(assigns) do
     ~F"""
     <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-      <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div class="flex items-start justify-center min-h-screen pt-4 px-4 text-center">
         <!-- Background overlay, show/hide based on modal state. -->
         <div :on-capture-click={@close} class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
         <!-- This element is to trick the browser into centering the modal contents. -->
-        <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <span class="hidden inline-block align-middle h-screen" aria-hidden="true">&#8203;</span>
 
         <!-- Modal panel, show/hide based on modal state. -->
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all mt-32 align-middle max-w-lg w-full">
+          <div class="bg-white px-4 pt-5 pb-4">
             <div class="flex items-start">
-              <div class="ml-4 text-left w-full">
+              <div class="mx-4 text-left w-full">
                 <div class="flex justify-between items-center">
                   <h3 class="text-lg leading-6 font-medium text-gray-600" id="modal-title">
                     {@title}
