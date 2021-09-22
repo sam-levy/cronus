2ecc36e30f1c2a05960da884eb977e22f1a8b2f9
Samuel Levy <samulevy@gmail.com>
2021-09-22 09:58:47 -0300

    Preload Account in list_by_entity_with_account

--- apps/sig/lib/sig/finance/banks/entity_bank_accounts.ex
+++ apps/sig/lib/sig/finance/banks/entity_bank_accounts.ex
@@ -4,10 +4,12 @@ defmodule Sig.Finance.Banks.EntityBankAccounts do
   alias Sig.Finance.Banks.EntityBankAccounts.EntityBankAccount
   alias Sig.Repo
 
-  def list_by_entity(org_id, entity_id) do
+  def list_by_entity_with_account(org_id, entity_id) do
     EntityBankAccount
     |> where(org_id: ^org_id)
     |> where(entity_id: ^entity_id)
+    |> join(:left, [eba], bank_account in assoc(eba, :bank_account))
+    |> preload([_eba, bank_account], bank_account: bank_account)
     |> order_by(:inserted_at)
     |> Repo.all()
   end
--- apps/sig/test/sig/finance/entity_bank_accounts_test.exs
+++ apps/sig/test/sig/finance/entity_bank_accounts_test.exs
@@ -1,11 +1,12 @@
 defmodule Sig.Finance.Banks.EntityBankAccountsTest do
   use Sig.DataCase
 
+  alias Sig.Finance.Banks.Accounts.Account
   alias Sig.Finance.Banks.EntityBankAccounts
   alias Sig.Finance.Banks.EntityBankAccounts.EntityBankAccount
 
-  describe "list_by_entity/2" do
-    test "lists EntityBankAccount by entity ordered by inserted_at" do
+  describe "list_by_entity_with_account/2" do
+    test "lists EntityBankAccount by entity ordered by inserted_at preloaded with Account" do
       %{id: org_id} = org = insert(:org)
       %{id: entity_id} = entity = insert(:entity, org: org)
 
@@ -22,21 +23,23 @@ defmodule Sig.Finance.Banks.EntityBankAccountsTest do
                %EntityBankAccount{
                  org_id: ^org_id,
                  entity_id: ^entity_id,
-                 bank_account_id: ^account_id_1
+                 bank_account_id: ^account_id_1,
+                 bank_account: %Account{}
                },
                %EntityBankAccount{
                  org_id: ^org_id,
                  entity_id: ^entity_id,
-                 bank_account_id: ^account_id_2
+                 bank_account_id: ^account_id_2,
+                 bank_account: %Account{}
                }
-             ] = EntityBankAccounts.list_by_entity(org.id, entity.id)
+             ] = EntityBankAccounts.list_by_entity_with_account(org.id, entity.id)
     end
 
     test "entity has no EntityBankAccount" do
       org = insert(:org)
       entity = insert(:entity, org: org)
 
-      assert EntityBankAccounts.list_by_entity(org.id, entity.id) == []
+      assert EntityBankAccounts.list_by_entity_with_account(org.id, entity.id) == []
     end
   end
 end
