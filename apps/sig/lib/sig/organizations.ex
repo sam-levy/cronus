600e2f23428a64dd052b50ce959cc6c95f084fe8
Samuel Levy <samulevy@gmail.com>
2021-09-22 16:33:13 -0300

    Add get_org

--- apps/sig/lib/sig/organizations.ex
+++ apps/sig/lib/sig/organizations.ex
@@ -2,10 +2,14 @@ defmodule Sig.Organizations do
   alias Sig.Organizations.Org
   alias Sig.Repo
 
-  def fetch_org(org_id) do
+  def fetch_org(org_id) when is_binary(org_id) do
     case Repo.get(Org, org_id) do
       %Org{} = org -> {:ok, org}
       nil -> {:error, :not_found}
     end
+  end
+
+  def get_org(org_id) when is_binary(org_id) do
+    Repo.get(Org, org_id)
   end
 end
