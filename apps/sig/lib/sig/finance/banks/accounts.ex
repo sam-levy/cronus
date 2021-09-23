e986df8d153cf572d489ca1e2afb38d2fd9915dd
Samuel Levy <samulevy@gmail.com>
2021-09-23 14:00:01 -0300

    Add Banks.Accounts.get_entity_primary/1

--- apps/sig/lib/sig/finance/banks/accounts.ex
+++ apps/sig/lib/sig/finance/banks/accounts.ex
@@ -6,10 +6,22 @@ defmodule Sig.Finance.Banks.Accounts do
   alias Sig.Repo
 
   def list_by_entity(%Entity{} = entity) do
+    entity
+    |> query_by_entity()
+    |> order_by(:inserted_at)
+    |> Repo.all()
+  end
+
+  def get_entity_primary(%Entity{} = entity) do
+    entity
+    |> query_by_entity()
+    |> where(is_primary: true)
+    |> Repo.one()
+  end
+
+  defp query_by_entity(entity) do
     Account
     |> where(org_id: ^entity.org_id)
     |> where(entity_id: ^entity.id)
-    |> order_by(:inserted_at)
-    |> Repo.all()
   end
 end
--- apps/sig/test/sig/finance/accounts_test.exs
+++ apps/sig/test/sig/finance/accounts_test.exs
@@ -4,7 +4,7 @@ defmodule Sig.Finance.Banks.AccountsTest do
   alias Sig.Finance.Banks.Accounts
   alias Sig.Finance.Banks.Accounts.Account
 
-  describe "list_by_entity/2" do
+  describe "list_by_entity/1" do
     test "lists bank accounts by entity ordered by inserted_at" do
       %{id: org_id} = org = insert(:org)
       %{id: entity_id} = entity = insert(:entity, org: org)
@@ -19,12 +19,12 @@ defmodule Sig.Finance.Banks.AccountsTest do
                %Account{
                  id: ^id_1,
                  org_id: ^org_id,
-                 entity_id: ^entity_id,
+                 entity_id: ^entity_id
                },
                %Account{
                  id: ^id_2,
                  org_id: ^org_id,
-                 entity_id: ^entity_id,
+                 entity_id: ^entity_id
                }
              ] = Accounts.list_by_entity(entity)
     end
@@ -33,6 +33,35 @@ defmodule Sig.Finance.Banks.AccountsTest do
       entity = insert(:entity)
 
       assert Accounts.list_by_entity(entity) == []
+    end
+  end
+
+  describe "get_entity_primary/1" do
+    test "gets the entity primary account" do
+      org = insert(:org)
+      entity = insert(:entity, org: org)
+
+      %{id: primary_account_id} =
+        insert(:bank_account, org: org, entity: entity, is_primary: true)
+
+      insert(:bank_account, org: org, entity: entity, is_primary: false)
+
+      assert %Account{id: ^primary_account_id} = Accounts.get_entity_primary(entity)
+    end
+
+    test "when entity doesn't have a primary account" do
+      org = insert(:org)
+      entity = insert(:entity, org: org)
+
+      insert(:bank_account, org: org, entity: entity, is_primary: false)
+
+      assert Accounts.get_entity_primary(entity) == nil
+    end
+
+    test "when entity doesn't have any account" do
+      entity = insert(:entity)
+
+      assert Accounts.get_entity_primary(entity) == nil
     end
   end
 end
