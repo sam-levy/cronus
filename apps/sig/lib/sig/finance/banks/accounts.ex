caf8ecbaf934dc47ec4e6d080a85d7e056bd66b1
Samuel Levy <samulevy@gmail.com>
2021-09-27 20:45:29 -0300

    Add Banks.Accounts context functions

--- apps/sig/lib/sig/finance/banks/accounts.ex
+++ apps/sig/lib/sig/finance/banks/accounts.ex
@@ -11,10 +11,18 @@ defmodule Sig.Finance.Banks.Accounts do
   defdelegate create(entity, attrs), to: Create, as: :call
   defdelegate update(account, attrs), to: Update, as: :call
 
+  def create_change(%{} = attrs \\ %{}) do
+    Account.create_changeset(attrs)
+  end
+
+  def update_change(%Account{} = account, %{} = attrs \\ %{}) do
+    Account.update_changeset(account, attrs)
+  end
+
   def list_by_entity(%Entity{} = entity) do
     entity
     |> query_by_entity()
-    |> order_by(:inserted_at)
+    |> order_by(:routing_number)
     |> Repo.all()
   end
 
@@ -22,7 +30,7 @@ defmodule Sig.Finance.Banks.Accounts do
     entity
     |> query_by_entity()
     |> where(is_active: true)
-    |> order_by(:inserted_at)
+    |> order_by(:routing_number)
     |> Repo.all()
   end
 
@@ -43,6 +51,28 @@ defmodule Sig.Finance.Banks.Accounts do
     |> Repo.one()
     |> handle_return()
   end
+
+  def fetch(%Entity{} = entity, id) do
+    entity
+    |> query_by_entity()
+    |> where(id: ^id)
+    |> Repo.one()
+    |> handle_return()
+  end
+
+  def subscribe_to_bank_accounts(%Entity{} = entity) do
+    Phoenix.PubSub.subscribe(Sig.PubSub, topic(entity))
+  end
+
+  def broadcast_bank_accounts(%Entity{} = entity) do
+    Phoenix.PubSub.broadcast(
+      Sig.PubSub,
+      topic(entity),
+      {:updated_bank_accounts, list_by_entity(entity)}
+    )
+  end
+
+  defp topic(%Entity{} = entity), do: "entity_id:" <> entity.id <> ":bank_accounts"
 
   defp query_by_entity(entity) do
     Account
--- apps/sig/test/sig/finance/accounts_test.exs
+++ apps/sig/test/sig/finance/accounts_test.exs
@@ -5,13 +5,28 @@ defmodule Sig.Finance.Banks.AccountsTest do
   alias Sig.Finance.Banks.Accounts
   alias Sig.Finance.Banks.Accounts.Account
 
+  @endpoint SigLive.Endpoint
+
+  describe "create_change/1" do
+    test "returns a changeset" do
+      assert %Ecto.Changeset{data: %Account{}} = Accounts.create_change()
+    end
+  end
+
+  describe "update_change/1" do
+    test "returns a changeset" do
+      assert %Ecto.Changeset{data: %Account{}} = Accounts.update_change(%Account{}, %{})
+      assert %Ecto.Changeset{data: %Account{}} = Accounts.update_change(%Account{})
+    end
+  end
+
   describe "list_by_entity/1" do
-    test "lists bank accounts by entity ordered by inserted_at" do
+    test "lists bank accounts by entity ordered by routing_number" do
       %{id: org_id} = org = insert(:org)
       %{id: entity_id} = entity = insert(:entity, org: org)
 
-      %{id: id_1} = insert(:bank_account, org: org, entity: entity)
-      %{id: id_2} = insert(:bank_account, org: org, entity: entity)
+      %{id: id_1} = insert(:bank_account, org: org, entity: entity, routing_number: "001")
+      %{id: id_2} = insert(:bank_account, org: org, entity: entity, routing_number: "104")
 
       _to_ignore_1 = insert(:bank_account, org: org)
       _to_ignore_2 = insert(:bank_account)
@@ -38,12 +53,12 @@ defmodule Sig.Finance.Banks.AccountsTest do
   end
 
   describe "list_active_by_entity/1" do
-    test "lists active bank accounts by entity ordered by inserted_at" do
+    test "lists active bank accounts by entity ordered by routing_number" do
       %{id: org_id} = org = insert(:org)
       %{id: entity_id} = entity = insert(:entity, org: org)
 
-      %{id: id_1} = insert(:bank_account, org: org, entity: entity, is_active: true)
-      %{id: id_2} = insert(:bank_account, org: org, entity: entity, is_active: true)
+      %{id: id_1} = insert(:bank_account, org: org, entity: entity, routing_number: "001", is_active: true)
+      %{id: id_2} = insert(:bank_account, org: org, entity: entity, routing_number: "104", is_active: true)
 
       _to_ignore_1 = insert(:bank_account, org: org, entity: entity, is_active: false)
       _to_ignore_2 = insert(:bank_account, org: org, is_active: true)
@@ -118,6 +133,63 @@ defmodule Sig.Finance.Banks.AccountsTest do
       org = insert(:org)
 
       assert Accounts.fetch_in_org_with_entity(org, UUID.generate()) == {:error, :not_found}
+    end
+  end
+
+  describe "fetch/2" do
+    test "fetches a bank account" do
+      %{id: id, entity: entity, entity_id: entity_id} = insert(:bank_account)
+
+      assert {:ok, %Account{id: ^id, entity_id: ^entity_id}} = Accounts.fetch(entity, id)
+    end
+
+    test "when bank account doesn't exist" do
+      entity = insert(:entity)
+
+      assert Accounts.fetch(entity, UUID.generate()) == {:error, :not_found}
+    end
+  end
+
+  describe "subscribe_to_bank_accounts/1" do
+    test "subscribes to bank accounts topic" do
+      entity = insert(:entity)
+      topic = "entity_id:" <> entity.id <> ":bank_accounts"
+
+      assert Accounts.subscribe_to_bank_accounts(entity) == :ok
+
+      Phoenix.PubSub.broadcast(
+        Sig.PubSub,
+        topic,
+        {:updated_bank_accounts, :bank_accounts}
+      )
+
+      assert_receive {:updated_bank_accounts, :bank_accounts}
+    end
+  end
+
+  describe "broadcast_bank_accounts/1" do
+    test "broadcasts bank accounts from an entity" do
+      org = insert(:org)
+      entity = insert(:entity, org: org)
+      accounts = insert_list(2, :bank_account, org: org, entity: entity)
+      _wrong_accounts = insert_list(2, :bank_account)
+
+      topic = "entity_id:" <> entity.id <> ":bank_accounts"
+
+      @endpoint.subscribe(topic)
+
+      assert Accounts.broadcast_bank_accounts(entity) == :ok
+
+      assert_receive {:updated_bank_accounts, received_accounts}
+
+      assert Enum.count(received_accounts) == 2
+
+      Enum.each(accounts, fn account ->
+        assert account.org_id == org.id
+        assert account.entity_id == entity.id
+      end)
+
+      @endpoint.unsubscribe(topic)
     end
   end
 end
