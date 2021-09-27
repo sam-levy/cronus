e5ce885b66946e8f723f4062f8352c7d038d12c6
Samuel Levy <samulevy@gmail.com>
2021-09-27 20:04:39 -0300

    Refactor Banks context

--- apps/sig/lib/sig/finance/banks.ex
+++ apps/sig/lib/sig/finance/banks.ex
@@ -16,18 +16,18 @@ defmodule Sig.Finance.Banks do
     }
   end)
 
-  @bank_names_by_routing_number Map.new(@all_banks, &{&1.routing_number, &1.name})
+  @banks_by_routing_number Map.new(@all_banks, &{&1.routing_number, &1})
 
   def list_banks, do: @all_banks
 
-  def fetch_bank_name(routing_number) do
-    case Map.get(@bank_names_by_routing_number, routing_number) do
-      name when is_binary(name) -> {:ok, name}
+  def fetch_bank(routing_number) do
+    case Map.get(@banks_by_routing_number, routing_number) do
+      %Bank{} = bank -> {:ok, bank}
       nil -> {:error, :not_found}
     end
   end
 
   def valid_routing_number?(routing_number) do
-    Map.has_key?(@bank_names_by_routing_number, routing_number)
+    Map.has_key?(@banks_by_routing_number, routing_number)
   end
 end
--- apps/sig/test/sig/finance/banks_test.exs
+++ apps/sig/test/sig/finance/banks_test.exs
@@ -10,17 +10,17 @@ defmodule Sig.Finance.BanksTest do
     end
   end
 
-  describe "fetch_bank_name/0" do
-    test "returns the bank name" do
-      assert Banks.fetch_bank_name("001") == {:ok, "Banco do Brasil S.A."}
+  describe "fetch_bank/1" do
+    test "returns a bank" do
+      assert {:ok, %Bank{name: "Banco do Brasil S.A."}} = Banks.fetch_bank("001")
     end
 
     test "invalid routing number" do
-      assert Banks.fetch_bank_name("invalid") == {:error, :not_found}
+      assert Banks.fetch_bank("invalid") == {:error, :not_found}
     end
   end
 
-  describe "valid_routing_number/0" do
+  describe "valid_routing_number/1" do
     test "when valid" do
       assert Banks.valid_routing_number?("001")
     end
