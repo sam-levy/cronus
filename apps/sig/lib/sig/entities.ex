b54d3c817ba97c6b799730b9310dbf604cb31f2b
Samuel Levy <samulevy@gmail.com>
2021-09-28 11:32:47 -0300

    Rename Entities facade delegates

--- apps/sig/lib/sig/entities.ex
+++ apps/sig/lib/sig/entities.ex
@@ -1,12 +1,12 @@
 defmodule Sig.Entities do
   alias Sig.Entities.Individuals
 
-  defdelegate individual_change(attrs), to: Individuals
-  defdelegate cast_individual_params(params), to: Individuals
-  defdelegate list_individuals(org_id), to: Individuals
-  defdelegate fetch_individual_by_cpf(org_id, cpf), to: Individuals
-  defdelegate create_individual(org_id, attrs), to: Individuals
+  defdelegate create_individual_change(attrs), to: Individuals
+  defdelegate list_individuals(org), to: Individuals
+  defdelegate get_individual(org, entity_id), to: Individuals
+  defdelegate fetch_individual_by_cpf(org, cpf), to: Individuals
+  defdelegate create_individual(org, attrs), to: Individuals
   defdelegate update_individual(individual, attrs), to: Individuals
-  defdelegate subscribe_to_individuals(org_id), to: Individuals
-  defdelegate broadcast_individuals(org_id), to: Individuals
+  defdelegate subscribe_to_individuals(org), to: Individuals
+  defdelegate broadcast_individuals(org), to: Individuals
 end
