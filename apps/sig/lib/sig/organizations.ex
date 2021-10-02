347ec32615f760b170647c996361a98e43652949
Samuel Levy <samulevy@gmail.com>
2021-10-01 22:31:38 -0300

    add list_sectors and list_positions Organizations context functions

--- apps/sig/lib/sig/organizations.ex
+++ apps/sig/lib/sig/organizations.ex
@@ -1,5 +1,9 @@
 defmodule Sig.Organizations do
+  import Ecto.Query
+
   alias Sig.Organizations.Org
+  alias Sig.Organizations.Position
+  alias Sig.Organizations.Sector
   alias Sig.Repo
 
   def fetch_org(org_id) when is_binary(org_id) do
@@ -11,5 +15,19 @@ defmodule Sig.Organizations do
 
   def get_org(org_id) when is_binary(org_id) do
     Repo.get(Org, org_id)
+  end
+
+  def list_sectors(%Org{} = org) do
+    Sector
+    |> where(org_id: ^org.id)
+    |> order_by(:name)
+    |> Repo.all()
+  end
+
+  def list_positions(%Org{} = org) do
+    Position
+    |> where(org_id: ^org.id)
+    |> order_by(:name)
+    |> Repo.all()
   end
 end
