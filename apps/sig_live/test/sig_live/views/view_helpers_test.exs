e71e83bcbccf368021c09997e1ce430cb9ef72c2
Samuel Levy <samulevy@gmail.com>
2021-10-03 20:11:10 -0300

    Show latest salary in EmployeeRegistrations.List

--- apps/sig_live/test/sig_live/views/view_helpers_test.exs
+++ apps/sig_live/test/sig_live/views/view_helpers_test.exs
@@ -135,4 +135,11 @@ defmodule SigLive.ViewHelpersTest do
       assert ViewHelpers.id_by_name_for_select([]) == %{}
     end
   end
+
+  describe "format_amount/1" do
+    test "formats amount" do
+      assert ViewHelpers.format_amount(%Money{amount: 1_500_00}) == "1.500,00"
+      assert ViewHelpers.format_amount(nil) == ""
+    end
+  end
 end
