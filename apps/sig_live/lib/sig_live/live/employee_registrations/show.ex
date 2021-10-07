9f9016adde6a6df82330a349b8ed1c9b9c839e0a
Samuel Levy <samulevy@gmail.com>
2021-10-07 17:27:33 -0300

    Add EmployeeRegistrations.Show component

--- apps/sig_live/lib/sig_live/live/employee_registrations/show.ex
+++ apps/sig_live/lib/sig_live/live/employee_registrations/show.ex
@@ -68,7 +68,6 @@ defmodule SigLive.EmployeeRegistrations.Show do
     {:noreply, assign(socket, suspensions: suspensions)}
   end
 
-  @impl true
   def handle_info({:updated_registration_leave_periods, leave_periods}, socket) do
     {:noreply, assign(socket, leave_periods: leave_periods)}
   end
