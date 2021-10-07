339dbf4bf9d15af0d2da482f5ecdf885d1099cd1
Samuel Levy <samulevy@gmail.com>
2021-10-07 12:33:35 -0300

    Add Warnings subcontext and functions

---
+++ apps/sig/lib/sig/hr/registrations/warnings.ex
@@ -0,0 +1,63 @@
+defmodule Sig.HR.Registrations.Warnings do
+  import Ecto.Query
+
+  alias Sig.HR.Registrations.Registration
+  alias Sig.HR.Registrations.Warnings.Warning
+  alias Sig.Repo
+
+  def create_change(%{} = attrs \\ %{}), do: Warning.create_changeset(attrs)
+
+  def update_change(%Warning{} = warning, %{} = attrs \\ %{}) do
+    Warning.update_changeset(warning, attrs)
+  end
+
+  def get(%Registration{} = registration, id) when is_binary(id) do
+    registration
+    |> query_by_registration()
+    |> where(id: ^id)
+    |> Repo.one()
+  end
+
+  def list_by_registration(%Registration{} = registration) do
+    registration
+    |> query_by_registration()
+    |> order_by(:date)
+    |> Repo.all
+  end
+
+  def create(%Registration{} = registration, %{} = attrs) do
+    attrs
+    |> Map.put(:org_id, registration.org_id)
+    |> Map.put(:registration_id, registration.id)
+    |> Warning.create_changeset()
+    |> Repo.insert()
+  end
+
+  def update(%Warning{} = warning, %{} = attrs) do
+    warning
+    |> Warning.update_changeset(attrs)
+    |> Repo.update()
+  end
+
+  def subscribe_to_registration_warnings(%Registration{} = registration) do
+    Phoenix.PubSub.subscribe(Sig.PubSub, topic(registration))
+  end
+
+  def broadcast_registration_warnings(%Registration{} = registration) do
+    Phoenix.PubSub.broadcast(
+      Sig.PubSub,
+      topic(registration),
+      {:updated_registration_warnings, list_by_registration(registration)}
+    )
+  end
+
+  defp topic(%Registration{} = registration) do
+    "registration_id:" <> registration.id <> ":warnings"
+  end
+
+  defp query_by_registration(registration) do
+    Warning
+    |> where(org_id: ^registration.org_id)
+    |> where(registration_id: ^registration.id)
+  end
+end
---
+++ apps/sig/test/sig/hr/registrations/warnings_test.exs
@@ -0,0 +1,171 @@
+defmodule Sig.HR.Registrations.WarningsTest do
+  use Sig.DataCase
+
+  alias Sig.HR.Registrations.Warnings
+  alias Sig.HR.Registrations.Warnings.Warning
+
+  @endpoint SigLive.Endpoint
+
+  describe "create_change/1" do
+    test "returns a changeset" do
+      assert %Ecto.Changeset{data: %Warning{}} = Warnings.create_change()
+    end
+  end
+
+  describe "update_change/1" do
+    test "returns a changeset" do
+      assert %Ecto.Changeset{data: %Warning{}} = Warnings.update_change(%Warning{}, %{})
+      assert %Ecto.Changeset{data: %Warning{}} = Warnings.update_change(%Warning{})
+    end
+  end
+
+  describe "get/2" do
+    test "gets a warning" do
+      registration = insert(:employee_registration)
+      %{id: id} = insert(:employee_warning, org: registration.org, registration: registration)
+
+      assert %Warning{id: ^id} = Warnings.get(registration, id)
+    end
+
+    test "warning from another registration" do
+      org = insert(:org)
+      registration_1 = insert(:employee_registration, org: org)
+      registration_2 = insert(:employee_registration, org: org)
+
+      warning = insert(:employee_warning, org: org, registration: registration_1)
+
+      assert Warnings.get(registration_2, warning.id) == nil
+    end
+
+    test "warning doesn't exist" do
+      registration = insert(:employee_registration)
+
+      assert Warnings.get(registration, UUID.generate()) == nil
+    end
+  end
+
+  describe "list_by_registration/1" do
+    test "lists warnings by registration ordered by date" do
+      org = insert(:org)
+      registration = insert(:employee_registration, org: org)
+
+      insert(:employee_warning, org: org, registration: registration, date: ~D[2020-01-01])
+      insert(:employee_warning, org: org, registration: registration, date: ~D[2020-06-01])
+
+      assert [
+               %Warning{date: ~D[2020-01-01]},
+               %Warning{date: ~D[2020-06-01]}
+             ] = Warnings.list_by_registration(registration)
+    end
+
+    test "registration has no warning" do
+      org = insert(:org)
+      registration = insert(:employee_registration, org: org)
+
+      assert Warnings.list_by_registration(registration) == []
+    end
+  end
+
+  describe "create/1" do
+    test "creates a warning" do
+      registration = insert(:employee_registration)
+
+      attrs = %{
+        org_id: registration.org_id,
+        registration_id: registration.id,
+        date: Date.utc_today(),
+        description: Faker.Lorem.paragraph(1)
+      }
+
+      assert {:ok, %Warning{id: id}} = Warnings.create(registration, attrs)
+
+      assert Repo.get_by(Warning,
+        id: id,
+        org_id: registration.org_id,
+        registration_id: registration.id,
+        date: attrs[:date],
+        description: attrs[:description],
+      )
+    end
+
+    test "returns changeset errors" do
+      registration = insert(:employee_registration)
+
+      assert {:error, changeset} = Warnings.create(registration, %{})
+
+      assert errors_on(changeset) == %{
+        date: ["can't be blank"],
+        description: ["can't be blank"]
+      }
+    end
+  end
+
+  describe "update/2" do
+    test "updates a warning" do
+      warning =
+        insert(:employee_warning,
+          date: ~D[2020-01-01],
+          description: "Putting the beans before the rice"
+        )
+
+      attrs = %{date: ~D[2021-01-02], description: "Pouring the beans before the rice"}
+
+      assert {:ok, _return} = Warnings.update(warning, attrs)
+
+      assert Repo.get_by(Warning,
+               id: warning.id,
+               org_id: warning.org_id,
+               registration_id: warning.registration_id,
+               date: attrs[:date],
+               description: attrs[:description]
+             )
+    end
+  end
+
+  describe "subscribe_to_registration_warnings/1" do
+    test "subscribes to registration warnings topic" do
+      registration = insert(:employee_registration)
+      topic = "registration_id:" <> registration.id <> ":warnings"
+
+      assert Warnings.subscribe_to_registration_warnings(registration) == :ok
+
+      Phoenix.PubSub.broadcast(
+        Sig.PubSub,
+        topic,
+        {:updated_registration_warnings, :warnings}
+      )
+
+      assert_receive {:updated_registration_warnings, :warnings}
+    end
+  end
+
+  describe "broadcast_registration_warnings/1" do
+    test "broadcasts warnings from a registration" do
+      org = insert(:org)
+      registration = insert(:employee_registration, org: org)
+
+      insert(:employee_warning, type: :transport, org: org, registration: registration)
+      insert(:employee_warning, type: :meal, org: org, registration: registration)
+
+      insert(:employee_warning, type: :transport)
+      insert(:employee_warning, type: :meal)
+
+      topic = "registration_id:" <> registration.id <> ":warnings"
+
+      @endpoint.subscribe(topic)
+
+      assert Warnings.broadcast_registration_warnings(registration) == :ok
+
+      assert_receive {:updated_registration_warnings, received_warnings}
+
+      assert Enum.count(received_warnings) == 2
+
+      Enum.each(received_warnings, fn warning ->
+        assert warning.org_id == org.id
+        assert warning.registration_id == registration.id
+      end)
+
+      @endpoint.unsubscribe(topic)
+    end
+  end
+end
