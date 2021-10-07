fa24d7e1e496f85a148324ecb26a648bab0a5a1a
Samuel Levy <samulevy@gmail.com>
2021-10-07 15:54:12 -0300

    Add Sig.Changeset.validate_dates/4

--- apps/sig/test/sig/changeset_test.exs
+++ apps/sig/test/sig/changeset_test.exs
@@ -285,6 +285,174 @@ defmodule Sig.ChangesetTest do
     end
   end
 
+  describe "validate_first_date_before_second/4" do
+    test "when first date is before second date" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2020-01-01], second_date: ~D[2021-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, :lt, :second_date)
+
+      assert changeset.valid?
+    end
+
+    test ":lt false" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2021-01-01], second_date: ~D[2020-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, :lt, :second_date)
+
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{first_date: ["must be before second_date"]}
+    end
+
+    test ":eq true" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2021-01-01], second_date: ~D[2021-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, :eq, :second_date)
+
+      assert changeset.valid?
+    end
+
+    test ":eq false" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2021-01-01], second_date: ~D[2020-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, :eq, :second_date)
+
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{first_date: ["must be equal to second_date"]}
+    end
+
+    test ":gt true" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2021-01-01], second_date: ~D[2020-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, :gt, :second_date)
+
+      assert changeset.valid?
+    end
+
+    test ":gt false" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2020-01-01], second_date: ~D[2021-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, :gt, :second_date)
+
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{first_date: ["must be after second_date"]}
+    end
+
+    test "[:lt, :eq] true when :lt" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2020-01-01], second_date: ~D[2021-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, [:lt, :eq], :second_date)
+
+      assert changeset.valid?
+    end
+
+    test "[:lt, :eq] true when :eq" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2021-01-01], second_date: ~D[2021-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, [:lt, :eq], :second_date)
+
+      assert changeset.valid?
+    end
+
+    test "[:lt, :eq] false when :gt" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2021-01-01], second_date: ~D[2020-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, [:lt, :eq], :second_date)
+
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{first_date: ["must be before or equal to second_date"]}
+    end
+
+    test "[:eq, :gt] true when :eq" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2021-01-01], second_date: ~D[2021-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, [:eq, :gt], :second_date)
+
+      assert changeset.valid?
+    end
+
+    test "[:eq, :gt] true when :gt" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2021-01-01], second_date: ~D[2020-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, [:eq, :gt], :second_date)
+
+      assert changeset.valid?
+    end
+
+    test "[:eq, :gt] false when :lt" do
+      data = %{}
+      types = %{first_date: :date, second_date: :date}
+      params = %{first_date: ~D[2020-01-01], second_date: ~D[2021-01-01]}
+
+      changeset =
+        {data, types}
+        |> Ecto.Changeset.cast(params, Map.keys(types))
+        |> Sig.Changeset.validate_dates(:first_date, [:eq, :gt], :second_date)
+
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{first_date: ["must be equal to or after second_date"]}
+    end
+  end
+
   describe "validate_money/2" do
     test "when amount is greater than 0" do
       data = %{}
