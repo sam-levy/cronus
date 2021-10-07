880c578b6a7adf9bd71227e5066c91eea37de834
Samuel Levy <samulevy@gmail.com>
2021-10-07 12:35:07 -0300

    Add EmployeeRegistrations.Warnings.List component

--- apps/sig_live/lib/sig_live/live/employee_registrations/show.ex
+++ apps/sig_live/lib/sig_live/live/employee_registrations/show.ex
@@ -51,7 +51,6 @@ defmodule SigLive.EmployeeRegistrations.Show do
     {:noreply, assign(socket, warnings: warnings)}
   end
 
-  @impl true
   def handle_info({:updated_registration_suspensions, suspensions}, socket) do
     {:noreply, assign(socket, suspensions: suspensions)}
   end
