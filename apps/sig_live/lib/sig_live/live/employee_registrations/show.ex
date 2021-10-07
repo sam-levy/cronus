51518e7be3123ff41cb21c94677d4036c3a5927a
Samuel Levy <samulevy@gmail.com>
2021-10-07 17:27:33 -0300

    Add EmployeeRegistrations.Show component

--- apps/sig_live/lib/sig_live/live/employee_registrations/show.ex
+++ apps/sig_live/lib/sig_live/live/employee_registrations/show.ex
@@ -8,6 +8,7 @@ defmodule SigLive.EmployeeRegistrations.Show do
   alias SigLive.EmployeeRegistrations.Salaries
   alias SigLive.EmployeeRegistrations.Vouchers
   alias SigLive.EmployeeRegistrations.Warnings
+  alias SigLive.EmployeeRegistrations.Suspensions
 
   @impl true
   def mount(%{"entity_id" => entity_id, "id" => id}, _session, socket) do
@@ -19,6 +20,7 @@ defmodule SigLive.EmployeeRegistrations.Show do
       HR.subscribe_to_registration_salaries(registration)
       HR.subscribe_to_registration_vouchers(registration)
       HR.subscribe_to_registration_warnings(registration)
+      HR.subscribe_to_registration_suspensions(registration)
     end
 
     socket =
@@ -27,10 +29,11 @@ defmodule SigLive.EmployeeRegistrations.Show do
         registration: registration,
         salaries: HR.list_salaries_by_registration(registration),
         vouchers: HR.list_vouchers_by_registration(registration),
-        warnings: HR.list_warnings_by_registration(registration)
+        warnings: HR.list_warnings_by_registration(registration),
+        suspensions: HR.list_suspensions_by_registration(registration)
       )
 
-    {:ok, socket, temporary_assigns: [salaries: [], vouchers: []]}
+    {:ok, socket, temporary_assigns: [salaries: [], vouchers: [], warnings: [], suspensions: []]}
   end
 
   @impl true
@@ -49,6 +52,11 @@ defmodule SigLive.EmployeeRegistrations.Show do
   end
 
   @impl true
+  def handle_info({:updated_registration_suspensions, suspensions}, socket) do
+    {:noreply, assign(socket, suspensions: suspensions)}
+  end
+
+  @impl true
   def handle_info({:flash, type, message}, socket) do
     {:noreply, put_flash(socket, type, message)}
   end
@@ -60,6 +68,7 @@ defmodule SigLive.EmployeeRegistrations.Show do
       <Salaries.List id="voucher_list" {=@registration} {=@salaries}/>
       <Vouchers.List id="salary_list" {=@registration} {=@vouchers}/>
       <Warnings.List id="warning_list" {=@registration} {=@warnings}/>
+      <Suspensions.List id="suspension_list" {=@registration} {=@suspensions}/>
     </div>
     """
   end
