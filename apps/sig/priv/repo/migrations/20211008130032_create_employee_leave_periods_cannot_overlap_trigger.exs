34bfc627cf50e3d9f0ff7032f70bfe3589ca03fb
Samuel Levy <samulevy@gmail.com>
2021-10-08 22:43:55 -0300

    Extract trigger function to its own migration file for reusability

--- apps/sig/priv/repo/migrations/20211008130032_create_employee_leave_periods_cannot_overlap_trigger.exs
+++ apps/sig/priv/repo/migrations/20211008130032_create_employee_leave_periods_cannot_overlap_trigger.exs
@@ -3,66 +3,14 @@ defmodule Sig.Repo.Migrations.CreateEmployeeLeavePeriodsCannotOverlapTrigger do
 
   def up do
     execute("""
-    CREATE OR REPLACE FUNCTION employee_leave_periods_cannot_overlap ()
-      RETURNS TRIGGER
-      LANGUAGE PLPGSQL
-      AS
-    $$
-    DECLARE
-      previous_end_date date;
-      next_start_date date;
-    BEGIN
-      SELECT
-        MAX (end_date)
-      FROM
-        employee_leave_periods
-      WHERE
-        id != NEW.id AND
-        org_id = NEW.org_id AND
-        registration_id = NEW.registration_id AND
-        start_date <= NEW.start_date
-      INTO previous_end_date;
-
-      SELECT
-        MIN (start_date)
-      FROM
-        employee_leave_periods
-      WHERE
-        id != NEW.id AND
-        org_id = NEW.org_id AND
-        registration_id = NEW.registration_id AND
-        start_date > NEW.start_date
-      INTO next_start_date;
-
-      IF
-        previous_end_date >= NEW.start_date
-      THEN
-        RAISE 'start_date before or equal to an existing record end_date'
-        USING ERRCODE = 'integrity_constraint_violation';
-      END IF;
-
-      IF
-        next_start_date <= NEW.end_date
-      THEN
-        RAISE 'end_date after or equal to an existing record start_date'
-        USING ERRCODE = 'integrity_constraint_violation';
-      END IF;
-
-      RETURN NEW;
-    END;
-    $$
-    """)
-
-    execute("""
     CREATE TRIGGER employee_leave_periods_cannot_overlap
       BEFORE INSERT OR UPDATE ON employee_leave_periods
       FOR EACH ROW
-      EXECUTE PROCEDURE employee_leave_periods_cannot_overlap ();
+      EXECUTE PROCEDURE ensure_no_period_overlap ();
     """)
   end
 
   def down do
     execute("DROP TRIGGER employee_leave_periods_cannot_overlap ON employee_leave_periods;")
-    execute("DROP FUNCTION employee_leave_periods_cannot_overlap;")
   end
 end
