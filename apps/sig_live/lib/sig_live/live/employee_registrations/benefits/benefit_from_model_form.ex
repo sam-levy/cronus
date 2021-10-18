687daa44fd6adeebce29ea956969d103e764f427
Samuel Levy <samulevy@gmail.com>
2021-10-18 10:59:36 -0300

    Add BenefitFromModelForm

---
+++ apps/sig_live/lib/sig_live/live/employee_registrations/benefits/benefit_from_model_form.ex
@@ -0,0 +1,190 @@
+defmodule SigLive.EmployeeRegistrations.Benefits.BenefitFromModelForm do
+  use SigLive, :surface_live_component
+
+  alias Sig.HR
+
+  alias Surface.Components.Form
+
+  alias Surface.Components.Form.{
+    TextInput,
+    Select,
+    Checkbox,
+    ErrorTag,
+    Field,
+    Label,
+    Submit,
+    DateInput
+  }
+
+  alias SigLive.Components.Modal
+
+  @form_states [:new_mode, :show_mode, :closed]
+
+  prop close_event, :event, required: true
+  prop close_fun, :fun, required: true
+  prop form_state, :atom, required: true, values!: @form_states
+  prop registration, :struct, required: true
+  prop benefit_id, :string, default: nil
+
+  data message, :string, default: nil
+
+  @impl true
+  def update(assigns, socket) do
+    %{registration: registration, benefit_id: benefit_id} = assigns
+    benefit = get_benefit(registration, benefit_id)
+
+    socket =
+      socket
+      |> assign(assigns)
+      |> assign(
+        benefit: benefit,
+        changeset: set_changeset(benefit),
+        benefit_models: HR.list_benefit_models(registration.org)
+      )
+
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("save", %{"benefit" => params}, socket) do
+    %{params: params, form_state: socket.assigns.form_state, socket: socket}
+    |> validate_params()
+    |> persist()
+    |> handle_return()
+  end
+
+  @impl true
+  def render(assigns) do
+    ~F"""
+    <Modal title={handle_title(@form_state)} close={@close_event}>
+      <Form for={@changeset} submit="save" opts={autocomplete: "off"}>
+        <Field name={:start_date} class="form-field">
+          <Label class="form-label">Data de Início</Label>
+          <DateInput {...props_for(:start_date, @form_state)}/>
+          <ErrorTag class="form-error-tag"/>
+        </Field>
+
+        <Field name={:benefit_model_id} class="form-field">
+          <Label class="form-label">Modelo</Label>
+          <Select
+            prompt=""
+            options={benefit_models_for_select(@benefit_models)}
+            {...props_for(:benefit_model_id, @form_state)}
+          />
+          <ErrorTag class="form-error-tag"/>
+        </Field>
+
+        <Field name={:end_date} :if={@form_state != :new_mode} class="form-field">
+          <Label class="form-label">Data de Término</Label>
+          <DateInput {...props_for(:end_date, @form_state)}/>
+          <ErrorTag class="form-error-tag"/>
+        </Field>
+
+        <Field name={:description} class="form-field">
+          <Label class="form-label">
+            Descrição
+
+            <span :if={@form_state != :show_mode} class="form-label-complement">
+              (opcional)
+            </span>
+          </Label>
+          <TextInput {...props_for(:description, @form_state)}/>
+          <ErrorTag class="form-error-tag"/>
+        </Field>
+
+        <Field name={:is_for_dependent} class="form-checkbox-field">
+          <Checkbox {...props_for_checkbox(:is_for_dependent, @form_state)}/>
+          <Label class="form-side-label">Para Dependente</Label>
+          <ErrorTag class="form-error-tag"/>
+        </Field>
+
+        <div :if={@message} class="form-error-tag mb-3">{@message}</div>
+
+        <div :if={@form_state != :show_mode} class="flex justify-end">
+          <Submit class="btn-blue" label="Salvar" opts={phx_disable_with: "Salvando..."}/>
+        </div>
+      </Form>
+    </Modal>
+    """
+  end
+
+  def states, do: @form_states
+
+  defp get_benefit(_registration, nil), do: nil
+  defp get_benefit(registration, benefit_id), do: HR.get_benefit(registration, benefit_id)
+
+  defp set_changeset(nil), do: HR.create_benefit_from_model_change()
+  defp set_changeset(benefit), do: HR.update_benefit_change(benefit)
+
+  defp validate_params(%{form_state: :new_mode} = context) do
+    changeset =
+      context.params
+      |> Map.put("org_id", "org_id")
+      |> Map.put("registration_id", "registration_id")
+      |> HR.create_benefit_from_model_change()
+
+    case apply_action(changeset, :insert) do
+      {:error, changeset} -> Map.put(context, :validation, {:error, changeset})
+      {:ok, _schema} -> Map.put(context, :validation, {:ok, changeset})
+    end
+  end
+
+  defp persist(%{validation: {:error, _}} = context), do: context
+
+  defp persist(%{validation: {:ok, changeset}, form_state: :new_mode} = context) do
+    %{registration: registration} = context.socket.assigns
+
+    Map.put(context, :return, HR.create_benefit_from_model(registration, changeset.changes))
+  end
+
+  defp handle_return(%{validation: {:error, changeset}, socket: socket}) do
+    {:noreply, assign(socket, message: nil, changeset: changeset)}
+  end
+
+  defp handle_return(%{return: {:error, message}} = context) when is_binary(message) do
+    {_, changeset} = context.validation
+
+    {:noreply, assign(context.socket, message: message, changeset: changeset)}
+  end
+
+  defp handle_return(%{return: {:error, changeset}} = context) when is_struct(changeset) do
+    {:noreply, assign(context.socket, message: nil, changeset: changeset)}
+  end
+
+  defp handle_return(%{return: {:ok, _benefit}, socket: socket}) do
+    %{registration: registration, form_state: form_state, close_fun: close_fun} = socket.assigns
+
+    HR.broadcast_registration_benefits(registration)
+    HR.broadcast_registration_recurring_payslip_items(registration)
+
+    handle_flash(form_state)
+    close_fun.()
+
+    {:noreply, socket}
+  end
+
+  defp handle_flash(:new_mode), do: send(self(), {:flash, :info, "Benefício criado"})
+
+  defp handle_title(:new_mode), do: "Adicionar Benefício"
+  defp handle_title(:show_mode), do: "Benefício"
+
+  @input_enabled [opts: [disabled: false], class: ["form-input"]]
+  @input_disabled [opts: [disabled: true], class: ["form-input-disabled"]]
+
+  @checkbox_enabled [opts: [disabled: false], class: ["form-checkbox"]]
+  @checkbox_disabled [opts: [disabled: true], class: ["form-checkbox-disabled"]]
+
+  defp props_for(_field, :new_mode), do: @input_enabled
+  defp props_for(_field, _form_state), do: @input_disabled
+
+  defp props_for_checkbox(:is_for_dependent, :new_mode), do: @checkbox_enabled
+  defp props_for_checkbox(:is_for_dependent, _form_state), do: @checkbox_disabled
+
+  defp benefit_models_for_select(benefit_models) do
+    Map.new(benefit_models, fn model ->
+      key = "#{model.type} - #{model.description} - #{Money.to_string(model.amount)}"
+
+      {key, model.id}
+    end)
+  end
+end
--- apps/sig_live/lib/sig_live/live/employee_registrations/benefits/list.ex
+++ apps/sig_live/lib/sig_live/live/employee_registrations/benefits/list.ex
@@ -1,14 +1,16 @@
 defmodule SigLive.EmployeeRegistrations.Benefits.List do
   use SigLive, :surface_live_component
 
-  alias SigLive.Components.ButtonPlus
+  alias SigLive.Components.DropdownBtn
   alias SigLive.Components.DropdownOpts
   alias SigLive.EmployeeRegistrations.Benefits.Form
+  alias SigLive.EmployeeRegistrations.Benefits.BenefitFromModelForm
 
   prop registration, :struct, required: true
   prop benefits, :list, required: true
 
   data form_state, :atom, default: :closed, values!: Form.states()
+  data benefit_from_model_form_state, :atom, default: :closed, values!: BenefitFromModelForm.states()
   data benefit_id, :string, default: nil
 
   @impl true
@@ -24,6 +26,16 @@ defmodule SigLive.EmployeeRegistrations.Benefits.List do
   @impl true
   def handle_event("open_show_benefit_form", %{"benefit-id" => id}, socket) do
     {:noreply, assign(socket, form_state: :show_mode, benefit_id: id)}
+  end
+
+  @impl true
+  def handle_event("open_new_benefit_from_model_form", _, socket) do
+    {:noreply, assign(socket, benefit_from_model_form_state: :new_mode)}
+  end
+
+  @impl true
+  def handle_event("open_show_benefit_from_model_form", %{"benefit-id" => id}, socket) do
+    {:noreply, assign(socket, benefit_from_model_form_state: :show_mode, benefit_id: id)}
   end
 
   @impl true
@@ -45,6 +57,16 @@ defmodule SigLive.EmployeeRegistrations.Benefits.List do
         {=@benefit_id}
       />
 
+      <BenefitFromModelForm
+        :if={@benefit_from_model_form_state != :closed}
+        id="benefit_from_model_form"
+        close_event="close_form"
+        close_fun={fn -> close_form(@id) end}
+        form_state={@benefit_from_model_form_state}
+        {=@registration}
+        {=@benefit_id}
+      />
+
       <table class="w-full bg-white shadow-lg my-7">
         <thead class="sticky top-0 z-20">
           <tr class="bg-white">
@@ -54,7 +76,10 @@ defmodule SigLive.EmployeeRegistrations.Benefits.List do
                   Benefícios
                 </span>
 
-                <ButtonPlus value="Adicionar" on_click="open_new_benefit_form"/>
+                <DropdownBtn text="Adicionar">
+                  <a :on-click="open_new_benefit_form" class="dropdown-item">Novo benefício</a>
+                  <a :on-click="open_new_benefit_from_model_form" class="dropdown-item">Benefício a partir de modelo</a>
+                </DropdownBtn>
               </div>
             </th>
           </tr>
@@ -77,7 +102,7 @@ defmodule SigLive.EmployeeRegistrations.Benefits.List do
             <tr class="border-b border-gray-200 hover:bg-gray-50">
               <td class="py-3 px-6 text-left">
                 <a
-                  :on-click="open_show_benefit_form"
+                  :on-click={if benefit.is_from_model, do: "open_show_benefit_from_model_form", else: "open_show_benefit_form"}
                   phx-value-benefit_id={benefit.id}
                   class="cursor-pointer hover:underline"
                 >
@@ -122,6 +147,6 @@ defmodule SigLive.EmployeeRegistrations.Benefits.List do
 
   def close_form(id), do: send_update(__MODULE__, closed_state(id))
 
-  defp closed_state, do: [form_state: :closed, benefit_id: nil]
   defp closed_state(id), do: closed_state() ++ [id: id]
+  defp closed_state, do: [form_state: :closed, benefit_from_model_form_state: :closed, benefit_id: nil]
 end
