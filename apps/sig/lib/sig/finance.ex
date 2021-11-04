5190611fc324268e9f56ecc9290221e713368f40
Samuel Levy <samulevy@gmail.com>
2021-11-04 12:22:31 -0300

    Add payables delegates to Finance context

--- apps/sig/lib/sig/finance.ex
+++ apps/sig/lib/sig/finance.ex
@@ -3,6 +3,7 @@ defmodule Sig.Finance do
   alias Sig.Finance.Banks
   alias Sig.Finance.Banks.Accounts
   alias Sig.Finance.Banks.EntityBankAccounts
+  alias Sig.Finance.Payables
 
   defdelegate list_banks, to: Banks
   defdelegate fetch_bank(routing_number), to: Banks
@@ -40,6 +41,19 @@ defmodule Sig.Finance do
 
   defdelegate entities_relationships(entity_1, entity_2), to: EntityBankAccounts
   defdelegate subscribe_to_entity_bank_accounts(entity), to: EntityBankAccounts
+
+  defdelegate authorize_payable(payable, attrs), to: Payables, as: :authorize
+  defdelegate unauthorize_payable(payable), to: Payables, as: :unauthorize
+  defdelegate create_payable_for_payslip(payslip, attrs), to: Payables
+  defdelegate delete_payable_for_payslip(payslip, payable), to: Payables
+  defdelegate set_payslip_payable_is_auto_adjustable_amount(payslip, payable_id), to: Payables
+  defdelegate create_payable_for_payslip_change(attrs \\ %{}), to: Payables
+  defdelegate update_payable_for_payslip_change(payable, attrs \\ %{}), to: Payables
+  defdelegate list_by_payslip(payslip), to: Payables
+  defdelegate get_by_payslip(payslip, id), to: Payables
+  defdelegate subscribe_to_payables_for_payslip(payslip), to: Payables
+  defdelegate unsubscribe_from_payables_for_payslip(payslip), to: Payables
+  defdelegate broadcast_payables_for_payslip(payslip), to: Payables
 
   def broadcast_accounts_and_relations(%Entity{} = entity) do
     Accounts.broadcast_bank_accounts(entity)
