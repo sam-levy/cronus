7663b020c6167a39bf644509c37b3c1d276af1f0
Samuel Levy <samulevy@gmail.com>
2021-11-09 15:09:38 -0300

    Refactor PayablesForPayslip.Create

--- apps/sig/lib/sig/finance/payables/payables_for_payslip/create.ex
+++ apps/sig/lib/sig/finance/payables/payables_for_payslip/create.ex
@@ -1,9 +1,10 @@
 defmodule Sig.Finance.Payables.PayablesForPayslip.Create do
-  import Ecto.Changeset, only: [add_error: 3, put_change: 3]
+  import Ecto.Changeset, only: [add_error: 3, apply_action: 2]
   import Ecto.Query
 
   alias Ecto.Multi
 
+  alias Sig.Changeset
   alias Sig.Finance.Payables.Payable
   alias Sig.Finance.Payables.PayablesForPayslip
   alias Sig.Finance.Payables.PayablesForPayslip.PayslipPayables
@@ -11,64 +12,80 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.Create do
   alias Sig.HR.Payslips.Payslip
   alias Sig.Repo
 
-  def call(%Payslip{} = payslip, %{} = attrs, opts \\ []) do
-    changeset = build_changeset(payslip, attrs, opts)
-
-    with %{valid?: true} <- changeset,
-         true <- valid_amount?(payslip, changeset, opts) do
-      Multi.new()
-      |> Multi.insert(:payable, changeset)
-      |> Multi.run(:payslip_payable, fn _, %{payable: payable} ->
-        PayslipPayables.create(payslip, payable, opts)
-      end)
-      |> Multi.run(:update_auto_adjustable_amount_payable, fn _, _ ->
-        PayablesForPayslip.update_auto_adjustable_amount_payable(payslip)
-      end)
-      |> Repo.transaction()
-      |> handle_return()
-    else
-      %{valid?: false} -> {:error, changeset}
-      false -> {:error, add_error(changeset, :amount, "can't exceed payslip amount")}
-    end
+  defmodule Context do
+    defstruct status: :ok,
+              opts: nil,
+              attrs: nil,
+              return: nil,
+              payslip: nil,
+              changeset: nil,
+              is_auto_adjustable_amount: nil
   end
 
-  defp build_changeset(payslip, attrs, opts) do
-    changeset =
+  def call(%Payslip{} = payslip, %{} = attrs, opts \\ []) do
+    %Context{
+      opts: opts,
+      attrs: attrs,
+      payslip: payslip,
+      is_auto_adjustable_amount: Keyword.get(opts, :is_auto_adjustable_amount, false)
+    }
+    |> build_changeset()
+    |> validate_amount()
+    |> validate_credit_bank_account()
+    |> validate_check_bank_account()
+    |> create_multi()
+    |> handle_return()
+  end
+
+  defp build_changeset(context) do
+    %{payslip: payslip, attrs: attrs} = context
+
     attrs
     |> Map.put(:org_id, payslip.org_id)
     |> Map.put(:target, :payslip)
+    |> Map.put_new(:reference_date, Date.beginning_of_month(payslip.start_date))
     |> Payable.create_changeset()
-
-    if Keyword.get(opts, :is_auto_adjustable_amount, false) do
-      put_change(changeset, :amount, nil)
-    else
-      changeset
+    |> handle_changeset_amount(context.is_auto_adjustable_amount)
+    |> case do
+      %{valid?: true} = changeset -> %{context | changeset: changeset}
+      changeset -> put_error(context, changeset)
     end
   end
 
-  defp valid_amount?(payslip, changeset, opts) do
-    if Keyword.get(opts, :is_auto_adjustable_amount, false) do
-      true
+  defp handle_changeset_amount(changeset, true), do: Changeset.drop_changes(changeset, :amount)
+  defp handle_changeset_amount(changeset, false), do: changeset
+
+  defp validate_amount(%{status: :halt} = context), do: context
+
+  defp validate_amount(%{is_auto_adjustable_amount: true} = context), do: context
+
+  defp validate_amount(context) do
+    %{payslip: payslip, changeset: changeset} = context
+
+    existing_non_adjustable_amount_sum = sum_non_adjustable_payables_amounts(payslip)
+    payslip = Payslips.get_by(org_id: payslip.org_id, id: payslip.id)
+
+    new_non_adjustable_amount_sum =
+      changeset.changes
+      |> Map.get(:amount, Money.new(0))
+      |> Money.add(existing_non_adjustable_amount_sum)
+
+    if payslip.amount >= new_non_adjustable_amount_sum do
+      context
     else
-      amount_sum = sum_non_adjustable_payables_amounts(payslip)
-      payslip = Payslips.get_by(org_id: payslip.org_id, id: payslip.id)
+      {:error, changeset} =
+        changeset
+        |> add_error(:amount, "can't exceed payslip amount")
+        |> apply_action(:update)
 
-      new_amount =
-        changeset.changes
-        |> Map.get(:amount, Money.new(0))
-        |> Money.add(amount_sum)
-
-      payslip.amount >= new_amount
+      put_error(context, changeset)
     end
   end
 
   def sum_non_adjustable_payables_amounts(payslip) do
     payslip
     |> PayablesForPayslip.query_by_payslip()
-    |> where(
-      [_payslip, payslip_payable: payslip_payable],
-      not payslip_payable.is_auto_adjustable_amount
-    )
+    |> where([payslip_payable: payslip_payable], not payslip_payable.is_auto_adjustable_amount)
     |> Repo.aggregate(:sum, :amount)
     |> case do
       %Money{} = sum -> sum
@@ -76,6 +93,70 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.Create do
     end
   end
 
-  defp handle_return({:ok, %{payable: payable}}), do: {:ok, payable}
-  defp handle_return({:error, _operation, reason, _changes}), do: {:error, reason}
+  defp validate_credit_bank_account(%{status: :halt} = context), do: context
+
+  defp validate_credit_bank_account(context) do
+    %{payslip: payslip, changeset: changeset} = context
+
+    case PayablesForPayslip.validate_credit_bank_account(changeset, payslip) do
+      {:ok, _} -> context
+      {:error, changeset} -> put_error(context, changeset)
+    end
+  end
+
+  defp validate_check_bank_account(%{status: :halt} = context), do: context
+
+  defp validate_check_bank_account(context) do
+    %{payslip: payslip, changeset: changeset} = context
+
+    case PayablesForPayslip.validate_check_bank_account(changeset, payslip) do
+      {:ok, _} -> context
+      {:error, changeset} -> put_error(context, changeset)
+    end
+  end
+
+  defp create_multi(%{status: :halt} = context), do: context
+
+  defp create_multi(context) do
+    %{payslip: payslip, changeset: changeset, opts: opts} = context
+
+    Multi.new()
+    |> Multi.run(:handle_non_auto_adjustable_amount, fn _, _ ->
+      handle_non_auto_adjustable_amount(context)
+    end)
+    |> Multi.insert(:payable, changeset)
+    |> Multi.run(:payslip_payable, fn _, %{payable: payable} ->
+      PayslipPayables.create(payslip, payable, opts)
+    end)
+    |> Multi.run(:handle_auto_adjustable_amount, fn _, _ ->
+      handle_auto_adjustable_amount(context)
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{payable: payable}} -> %{context | return: payable}
+      {:error, _operation, reason, _changes} -> put_error(context, reason)
+    end
+  end
+
+  defp handle_non_auto_adjustable_amount(
+         %{is_auto_adjustable_amount: false, changeset: %{changes: %{amount: amount}}} = context
+       )
+       when not is_nil(amount) do
+    %{payslip: payslip} = context
+
+    PayablesForPayslip.update_auto_adjustable_amount_payable(payslip, subtract: amount)
+  end
+
+  defp handle_non_auto_adjustable_amount(_context), do: {:ok, nil}
+
+  defp handle_auto_adjustable_amount(%{is_auto_adjustable_amount: true} = context) do
+    PayablesForPayslip.update_auto_adjustable_amount_payable(context.payslip)
+  end
+
+  defp handle_auto_adjustable_amount(_context), do: {:ok, nil}
+
+  defp put_error(context, error), do: %{context | status: :halt, return: {:error, error}}
+
+  defp handle_return(%{status: :ok, return: return}), do: {:ok, return}
+  defp handle_return(%{status: :halt, return: {:error, error}}), do: {:error, error}
 end
--- apps/sig/test/sig/finance/payables/payables_for_payslip/create_test.exs
+++ apps/sig/test/sig/finance/payables/payables_for_payslip/create_test.exs
@@ -32,8 +32,54 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.CreateTest do
       assert {:error, changeset} = Create.call(payslip, attrs, is_auto_adjustable_amount: false)
 
       assert errors_on(changeset) == %{
-        amount: ["can't exceed payslip amount"]
+               amount: ["can't exceed payslip amount"]
+             }
+    end
+
+    test "ignores the given amount when is auto adjustable" do
+      org = insert(:org)
+      payslip = insert(:payslip, org: org)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 500_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 500_00))
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        amount: 600_00,
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :cash
       }
+
+      assert {:ok, %Payable{} = return} =
+               Create.call(payslip, attrs, is_auto_adjustable_amount: true)
+
+      assert Repo.get_by(Payable,
+               org_id: payslip.org_id,
+               id: return.id,
+               target: :payslip,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method],
+               amount: 500_00
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: payslip.org_id,
+               payslip_id: payslip.id,
+               payable_id: return.id,
+               is_auto_adjustable_amount: true
+             )
     end
 
     test "when amount is not given" do
@@ -58,7 +104,8 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.CreateTest do
         method: :cash
       }
 
-      assert {:ok, %Payable{} = return} = Create.call(payslip, attrs, is_auto_adjustable_amount: false)
+      assert {:ok, %Payable{} = return} =
+               Create.call(payslip, attrs, is_auto_adjustable_amount: false)
 
       assert Repo.get_by(Payable,
                org_id: payslip.org_id,
@@ -69,7 +116,7 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.CreateTest do
                description: attrs[:description],
                note: attrs[:note],
                method: attrs[:method],
-               amount: 0,
+               amount: 0
              )
 
       assert Repo.get_by(PayslipPayable,
@@ -94,14 +141,24 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.CreateTest do
       # Update payslip amount
       Repo.update!(change(payslip, amount: 800_00))
 
-      existing_payable = insert(:payable_cash, org: org, target: :payslip, amount: 400_00)
+      non_adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 400_00)
 
       _existing_payslip_payable =
         insert(:payslip_payable,
           org: org,
           payslip: payslip,
-          payable: existing_payable,
+          payable: non_adjustable_payable,
           is_auto_adjustable_amount: false
+        )
+
+      adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 400_00)
+
+      _existing_payslip_payable =
+        insert(:payslip_payable,
+          org: org,
+          payslip: payslip,
+          payable: adjustable_payable,
+          is_auto_adjustable_amount: true
         )
 
       attrs = %{
@@ -116,8 +173,91 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.CreateTest do
       assert {:error, changeset} = Create.call(payslip, attrs, is_auto_adjustable_amount: false)
 
       assert errors_on(changeset) == %{
-        amount: ["can't exceed payslip amount"]
+               amount: ["can't exceed payslip amount"]
+             }
+    end
+
+    test "new fixed payable amount brings the existing adjustable amount to zero" do
+      org = insert(:org)
+      payslip = insert(:payslip, org: org)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 800_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 800_00))
+
+      non_adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 400_00)
+
+      _existing_payslip_payable =
+        insert(:payslip_payable,
+          org: org,
+          payslip: payslip,
+          payable: non_adjustable_payable,
+          is_auto_adjustable_amount: false
+        )
+
+      adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 400_00)
+
+      _existing_payslip_payable =
+        insert(:payslip_payable,
+          org: org,
+          payslip: payslip,
+          payable: adjustable_payable,
+          is_auto_adjustable_amount: true
+        )
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        amount: 400_00,
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :cash
       }
+
+      assert {:ok, %Payable{} = return} = Create.call(payslip, attrs)
+
+      assert Repo.get_by(Payable,
+               org_id: payslip.org_id,
+               id: return.id,
+               target: :payslip,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               amount: attrs[:amount],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method]
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: payslip.org_id,
+               payslip_id: payslip.id,
+               payable_id: return.id,
+               is_auto_adjustable_amount: false
+             )
+
+      assert Repo.get_by(Payable, org_id: org.id, id: non_adjustable_payable.id, amount: 400_00)
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: non_adjustable_payable.id,
+               is_auto_adjustable_amount: false
+             )
+
+      assert Repo.get_by(Payable, org_id: org.id, id: adjustable_payable.id, amount: 0_00)
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: adjustable_payable.id,
+               is_auto_adjustable_amount: true
+             )
     end
 
     test "creates a fixed payable when no payable exist" do
@@ -247,6 +387,87 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.CreateTest do
                payslip_id: payslip.id,
                payable_id: existing_payable_2.id,
                is_auto_adjustable_amount: false
+             )
+    end
+
+    test "creates a fixed payable when adjustable payables exist" do
+      org = insert(:org)
+      payslip = insert(:payslip, org: org)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 900_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 900_00))
+
+      non_adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 200_00)
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: non_adjustable_payable,
+        is_auto_adjustable_amount: false
+      )
+
+      adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 700_00)
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: adjustable_payable,
+        is_auto_adjustable_amount: true
+      )
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        amount: 400_00,
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :cash
+      }
+
+      assert {:ok, %Payable{} = return} = Create.call(payslip, attrs)
+
+      assert Repo.get_by(Payable,
+               org_id: payslip.org_id,
+               id: return.id,
+               target: :payslip,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               amount: attrs[:amount],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method]
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: payslip.org_id,
+               payslip_id: payslip.id,
+               payable_id: return.id,
+               is_auto_adjustable_amount: false
+             )
+
+      assert Repo.get_by(Payable, org_id: org.id, id: non_adjustable_payable.id, amount: 200_00)
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: non_adjustable_payable.id,
+               is_auto_adjustable_amount: false
+             )
+
+      assert Repo.get_by(Payable, org_id: org.id, id: adjustable_payable.id, amount: 300_00)
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: adjustable_payable.id,
+               is_auto_adjustable_amount: true
              )
     end
 
@@ -426,14 +647,289 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.CreateTest do
              )
     end
 
+    test "when credit bank account is NOT related to individual" do
+      org = insert(:org)
+      individual = insert(:individual, org: org)
+      registration = insert(:employee_registration, org: org, individual: individual)
+      payslip = insert(:payslip, org: org, registration: registration)
+
+      wrong_bank_account = insert(:bank_account, org: org)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 100_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 100_00))
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :bank_transfer,
+        credit_bank_account_id: wrong_bank_account.id,
+        amount: 100_00
+      }
+
+      assert {:error, changeset} = Create.call(payslip, attrs)
+
+      assert errors_on(changeset) == %{
+               credit_bank_account_id: ["isn't related to the individual"]
+             }
+    end
+
+    test "when credit bank account belongs to individual" do
+      org = insert(:org)
+      individual = insert(:individual, org: org)
+      registration = insert(:employee_registration, org: org, individual: individual)
+      payslip = insert(:payslip, org: org, registration: registration)
+      bank_account = insert(:bank_account, org: org, entity: individual.entity)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 100_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 100_00))
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :bank_transfer,
+        credit_bank_account_id: bank_account.id,
+        amount: 100_00
+      }
+
+      assert {:ok, %Payable{} = return} = Create.call(payslip, attrs)
+
+      assert Repo.get_by(Payable,
+               org_id: org.id,
+               id: return.id,
+               target: :payslip,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method],
+               credit_bank_account_id: attrs[:credit_bank_account_id],
+               amount: 100_00
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: return.id
+             )
+    end
+
+    test "when credit bank account is associated to individual" do
+      org = insert(:org)
+      individual = insert(:individual, org: org)
+      registration = insert(:employee_registration, org: org, individual: individual)
+      payslip = insert(:payslip, org: org, registration: registration)
+
+      bank_account = insert(:bank_account, org: org)
+
+      insert(:entity_bank_account, org: org, entity: individual.entity, bank_account: bank_account)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 100_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 100_00))
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :bank_transfer,
+        credit_bank_account_id: bank_account.id,
+        amount: 100_00
+      }
+
+      assert {:ok, %Payable{} = return} = Create.call(payslip, attrs)
+
+      assert Repo.get_by(Payable,
+               org_id: org.id,
+               id: return.id,
+               target: :payslip,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method],
+               credit_bank_account_id: attrs[:credit_bank_account_id],
+               amount: 100_00
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: return.id
+             )
+    end
+
+    test "when check bank account is NOT related to company" do
+      org = insert(:org)
+      company = insert(:company, org: org)
+      registration = insert(:employee_registration, org: org, registered_at: company)
+      payslip = insert(:payslip, org: org, registration: registration)
+
+      wrong_bank_account = insert(:bank_account, org: org)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 100_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 100_00))
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :check,
+        check_number: random_string_number(),
+        check_bank_account_id: wrong_bank_account.id,
+        amount: 100_00
+      }
+
+      assert {:error, changeset} = Create.call(payslip, attrs)
+
+      assert errors_on(changeset) == %{
+               check_bank_account_id: ["isn't related to the company"]
+             }
+    end
+
+    test "when check bank account belongs to company" do
+      org = insert(:org)
+      company = insert(:company, org: org)
+      registration = insert(:employee_registration, org: org, registered_at: company)
+      payslip = insert(:payslip, org: org, registration: registration)
+      bank_account = insert(:bank_account, org: org, entity: company.entity)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 100_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 100_00))
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :check,
+        check_number: random_string_number(),
+        check_bank_account_id: bank_account.id,
+        amount: 100_00
+      }
+
+      assert {:ok, %Payable{} = return} = Create.call(payslip, attrs)
+
+      assert Repo.get_by(Payable,
+               org_id: org.id,
+               id: return.id,
+               target: :payslip,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method],
+               check_number: attrs[:check_number],
+               check_bank_account_id: attrs[:check_bank_account_id],
+               amount: 100_00
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: return.id
+             )
+    end
+
+    test "when check bank account is associated to company" do
+      org = insert(:org)
+      company = insert(:company, org: org)
+      registration = insert(:employee_registration, org: org, registered_at: company)
+      payslip = insert(:payslip, org: org, registration: registration)
+
+      bank_account = insert(:bank_account, org: org)
+      insert(:entity_bank_account, org: org, entity: company.entity, bank_account: bank_account)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 100_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 100_00))
+
+      attrs = %{
+        due_date: Date.utc_today(),
+        reference_date: Date.utc_today() |> Date.beginning_of_month(),
+        description: Faker.Lorem.sentence(),
+        note: Faker.Lorem.sentence(),
+        method: :check,
+        check_number: random_string_number(),
+        check_bank_account_id: bank_account.id,
+        amount: 100_00
+      }
+
+      assert {:ok, %Payable{} = return} = Create.call(payslip, attrs)
+
+      assert Repo.get_by(Payable,
+               org_id: org.id,
+               id: return.id,
+               target: :payslip,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method],
+               check_number: attrs[:check_number],
+               check_bank_account_id: attrs[:check_bank_account_id],
+               amount: 100_00
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: return.id
+             )
+    end
+
     test "returns changeset errors" do
       payslip = insert(:payslip)
 
       assert {:error, changeset} = Create.call(payslip, %{})
 
       assert errors_on(changeset) == %{
-               due_date: ["can't be blank"],
-               reference_date: ["can't be blank"]
+               due_date: ["can't be blank"]
              }
 
       refute Repo.get_by(Payable, org_id: payslip.org_id)
