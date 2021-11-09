6e07d92985e2f222882dd700ae03ebd04b0e0899
Samuel Levy <samulevy@gmail.com>
2021-11-09 15:10:57 -0300

    Refactor PayablesForPayslip.Update

--- apps/sig/lib/sig/finance/payables/payables_for_payslip/update.ex
+++ apps/sig/lib/sig/finance/payables/payables_for_payslip/update.ex
@@ -1,9 +1,10 @@
 defmodule Sig.Finance.Payables.PayablesForPayslip.Update do
-  import Ecto.Changeset, only: [add_error: 3, put_change: 3]
+  import Ecto.Changeset, only: [add_error: 3, apply_action: 2]
   import Ecto.Query
 
   alias Ecto.Multi
 
+  alias Sig.Changeset
   alias Sig.Finance.Payables.Payable
   alias Sig.Finance.Payables.PayablesForPayslip
   alias Sig.Finance.Payables.PayablesForPayslip.PayslipPayables.PayslipPayable
@@ -11,64 +12,96 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.Update do
   alias Sig.HR.Payslips.Payslip
   alias Sig.Repo
 
+  defmodule Context do
+    defstruct status: :ok,
+              attrs: nil,
+              return: nil,
+              payslip: nil,
+              payable: nil,
+              changeset: nil,
+              is_auto_adjustable_amount: nil
+  end
+
   def call(%Payslip{} = payslip, %Payable{} = payable, %{} = attrs) do
-    payslip_payable =
-      Repo.get_by(PayslipPayable,
-        org_id: payslip.org_id,
-        payslip_id: payslip.id,
-        payable_id: payable.id
-      )
+    %Context{
+      attrs: attrs,
+      payslip: payslip,
+      payable: payable
+    }
+    |> set_is_auto_adjustable_amount()
+    |> build_changeset()
+    |> validate_amount()
+    |> validate_credit_bank_account()
+    |> validate_check_bank_account()
+    |> update_multi()
+    |> handle_return()
+  end
 
-    changeset = build_changeset(payable, attrs, payslip_payable)
+  defp set_is_auto_adjustable_amount(context) do
+    %{payslip: payslip, payable: payable} = context
 
-    with %{valid?: true} <- changeset,
-         true <- valid_amount?(payslip, changeset, payslip_payable) do
-      Multi.new()
-      |> Multi.update(:payable, changeset)
-      |> Multi.run(:update_auto_adjustable_amount_payable, fn _, _ ->
-        if payslip_payable.is_auto_adjustable_amount do
-          {:ok, nil}
-        else
-          PayablesForPayslip.update_auto_adjustable_amount_payable(payslip)
-        end
-      end)
-      |> Repo.transaction()
-      |> handle_return()
-    else
-      %{valid?: false} -> {:error, changeset}
-      false -> {:error, add_error(changeset, :amount, "can't exceed payslip amount")}
+    PayslipPayable
+    |> Repo.get_by(
+      org_id: payslip.org_id,
+      payslip_id: payslip.id,
+      payable_id: payable.id
+    )
+    |> case do
+      %PayslipPayable{} = payslip_payable ->
+        %{context | is_auto_adjustable_amount: payslip_payable.is_auto_adjustable_amount}
+
+      nil ->
+        put_error(context, "payable doesn't belong to payslip")
     end
   end
 
-  defp build_changeset(payable, attrs, %{is_auto_adjustable_amount: true}) do
-    payable
-    |> Payable.update_changeset(attrs)
-    |> put_change(:amount, nil)
+  defp build_changeset(%{status: :halt} = context), do: context
+
+  defp build_changeset(context) do
+    context.payable
+    |> Payable.update_changeset(context.attrs)
+    |> handle_changeset_amount(context.is_auto_adjustable_amount)
+    |> case do
+      %{valid?: true} = changeset -> %{context | changeset: changeset}
+      changeset -> put_error(context, changeset)
+    end
   end
 
-  defp build_changeset(payable, attrs, _payslip_payable) do
-    Payable.update_changeset(payable, attrs)
-  end
+  defp handle_changeset_amount(changeset, true), do: Changeset.drop_changes(changeset, :amount)
+  defp handle_changeset_amount(changeset, false), do: changeset
 
-  defp valid_amount?(_payslip, _changeset, %{is_auto_adjustable_amount: true}), do: true
+  defp validate_amount(%{status: :halt} = context), do: context
 
-  defp valid_amount?(payslip, changeset, payslip_payable) do
-    amount_sum = sum_non_adjustable_payables_amounts(payslip, payslip_payable.payable_id)
+  defp validate_amount(%{is_auto_adjustable_amount: true} = context), do: context
+
+  defp validate_amount(context) do
+    %{payslip: payslip, payable: payable, changeset: changeset} = context
+
+    existing_non_adjustable_amount_sum = sum_non_adjustable_payables_amounts(payslip, payable.id)
     payslip = Payslips.get_by(org_id: payslip.org_id, id: payslip.id)
 
-    new_amount =
+    new_non_adjustable_amount_sum =
       changeset.changes
       |> Map.get(:amount, Money.new(0))
-      |> Money.add(amount_sum)
+      |> Money.add(existing_non_adjustable_amount_sum)
 
-    payslip.amount >= new_amount
+    if payslip.amount >= new_non_adjustable_amount_sum do
+      context
+    else
+      {:error, changeset} =
+        changeset
+        |> add_error(:amount, "can't exceed payslip amount")
+        |> apply_action(:update)
+
+      put_error(context, changeset)
+    end
   end
 
   def sum_non_adjustable_payables_amounts(payslip, payable_id) do
     payslip
     |> PayablesForPayslip.query_by_payslip()
     |> where(
-      [_payslip, payslip_payable: payslip_payable],
+      [payslip_payable: payslip_payable],
       not payslip_payable.is_auto_adjustable_amount and
         payslip_payable.payable_id != ^payable_id
     )
@@ -79,6 +112,98 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.Update do
     end
   end
 
-  defp handle_return({:ok, %{payable: payable}}), do: {:ok, payable}
-  defp handle_return({:error, _operation, reason, _changes}), do: {:error, reason}
+  defp validate_credit_bank_account(%{status: :halt} = context), do: context
+
+  defp validate_credit_bank_account(context) do
+    %{payslip: payslip, changeset: changeset} = context
+
+    case PayablesForPayslip.validate_credit_bank_account(changeset, payslip) do
+      {:ok, _} -> context
+      {:error, changeset} -> put_error(context, changeset)
+    end
+  end
+
+  defp validate_check_bank_account(%{status: :halt} = context), do: context
+
+  defp validate_check_bank_account(context) do
+    %{payslip: payslip, changeset: changeset} = context
+
+    case PayablesForPayslip.validate_check_bank_account(changeset, payslip) do
+      {:ok, _} -> context
+      {:error, changeset} -> put_error(context, changeset)
+    end
+  end
+
+  defp update_multi(%{status: :halt} = context), do: context
+
+  defp update_multi(context) do
+    Multi.new()
+    |> Multi.run(:handle_non_auto_adjustable_amount_subtract, fn _, _ ->
+      handle_non_auto_adjustable_amount_subtract(context)
+    end)
+    |> Multi.update(:payable, context.changeset)
+    |> Multi.run(:handle_non_auto_adjustable_amount, fn _, _ ->
+      handle_non_auto_adjustable_amount(context)
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{payable: payable}} -> %{context | return: payable}
+      {:error, _operation, reason, _changes} -> put_error(context, reason)
+    end
+  end
+
+  defp handle_non_auto_adjustable_amount_subtract(
+         %{is_auto_adjustable_amount: false, changeset: %{changes: %{amount: changeset_amount}}} =
+           context
+       )
+       when not is_nil(changeset_amount) do
+    %{payslip: payslip, payable: payable} = context
+
+    case Money.compare(changeset_amount, payable.amount) do
+      -1 ->
+        {:ok, nil}
+
+      0 ->
+        {:ok, nil}
+
+      1 ->
+        amount_diff = Money.subtract(changeset_amount, payable.amount)
+
+        PayablesForPayslip.update_auto_adjustable_amount_payable(payslip, subtract: amount_diff)
+    end
+  end
+
+  defp handle_non_auto_adjustable_amount_subtract(_context), do: {:ok, nil}
+
+  defp handle_non_auto_adjustable_amount(
+         %{is_auto_adjustable_amount: false, changeset: %{changes: %{amount: changeset_amount}}} =
+           context
+       )
+       when not is_nil(changeset_amount) do
+    %{payslip: payslip, payable: payable} = context
+
+    case Money.compare(changeset_amount, payable.amount) do
+      -1 ->
+        PayablesForPayslip.update_auto_adjustable_amount_payable(payslip)
+
+      0 ->
+        {:ok, nil}
+
+      1 ->
+        {:ok, nil}
+    end
+  end
+
+  defp handle_non_auto_adjustable_amount(_context), do: {:ok, nil}
+
+  # defp handle_auto_adjustable_amount(%{is_auto_adjustable_amount: true} = context) do
+  #   PayablesForPayslip.update_auto_adjustable_amount_payable(context.payslip)
+  # end
+
+  # defp handle_auto_adjustable_amount(_context), do: {:ok, nil}
+
+  defp put_error(context, error), do: %{context | status: :halt, return: {:error, error}}
+
+  defp handle_return(%{status: :ok, return: return}), do: {:ok, return}
+  defp handle_return(%{status: :halt, return: {:error, error}}), do: {:error, error}
 end
--- apps/sig/test/sig/finance/payables/payables_for_payslip/update_test.exs
+++ apps/sig/test/sig/finance/payables/payables_for_payslip/update_test.exs
@@ -3,6 +3,7 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.UpdateTest do
 
   alias Sig.Finance.Payables.PayablesForPayslip.Update
   alias Sig.Finance.Payables.Payable
+  alias Sig.Finance.Payables.PayablesForPayslip.PayslipPayables.PayslipPayable
 
   describe "update/3 for Payslip" do
     test "updates a payable" do
@@ -19,8 +20,15 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.UpdateTest do
       # Update payslip amount
       Repo.update!(change(payslip, amount: 100_00))
 
-      %{id: id} = payable = insert(:payable_cash, org: org, target: :payslip, amount: 100_00)
-      insert(:payslip_payable, org: org, payslip: payslip, payable: payable, is_auto_adjustable_amount: false)
+      %{id: id} =
+        payable = insert(:payable_cash, org: org, target: :payslip, amount: Money.new(100_00))
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: payable,
+        is_auto_adjustable_amount: false
+      )
 
       attrs = %{
         due_date: ~D[2021-01-15],
@@ -31,7 +39,6 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.UpdateTest do
         method: :billet,
         billet_barcode: random_string_number()
       }
-
 
       assert {:ok, %Payable{id: ^id} = return} = Update.call(payslip, payable, attrs)
 
@@ -46,6 +53,158 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.UpdateTest do
                note: attrs[:note],
                method: attrs[:method],
                billet_barcode: attrs[:billet_barcode]
+             )
+    end
+
+    test "decreases an auto adjustable payable amount when a fixed payable amount is increased" do
+      org = insert(:org)
+      payslip = insert(:payslip, org: org)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 500_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 500_00))
+
+      %{id: id} =
+        payable = insert(:payable_cash, org: org, target: :payslip, amount: Money.new(100_00))
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: payable,
+        is_auto_adjustable_amount: false
+      )
+
+      adjustable_payable =
+        insert(:payable_cash, org: org, target: :payslip, amount: Money.new(400_00))
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: adjustable_payable,
+        is_auto_adjustable_amount: true
+      )
+
+      attrs = %{
+        due_date: ~D[2021-01-15],
+        reference_date: ~D[2021-01-01],
+        amount: 300_00,
+        description: "Updated description",
+        note: "Updated note",
+        method: :billet,
+        billet_barcode: random_string_number()
+      }
+
+      assert {:ok, %Payable{id: ^id} = return} = Update.call(payslip, payable, attrs)
+
+      assert Repo.get_by(Payable,
+               org_id: return.org_id,
+               id: id,
+               target: payable.target,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               amount: attrs[:amount],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method],
+               billet_barcode: attrs[:billet_barcode]
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: payslip.org_id,
+               payslip_id: payslip.id,
+               payable_id: return.id,
+               is_auto_adjustable_amount: false
+             )
+
+      assert Repo.get_by(Payable, org_id: org.id, id: adjustable_payable.id, amount: 200_00)
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: adjustable_payable.id,
+               is_auto_adjustable_amount: true
+             )
+    end
+
+    test "increases an auto adjustable payable amount when a fixed payable amount is decreased" do
+      org = insert(:org)
+      payslip = insert(:payslip, org: org)
+
+      insert(:payslip_outside_item,
+        org: org,
+        payslip: payslip,
+        outside_item_entry_type: :credit,
+        amount: 500_00
+      )
+
+      # Update payslip amount
+      Repo.update!(change(payslip, amount: 500_00))
+
+      %{id: id} =
+        payable = insert(:payable_cash, org: org, target: :payslip, amount: Money.new(200_00))
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: payable,
+        is_auto_adjustable_amount: false
+      )
+
+      adjustable_payable =
+        insert(:payable_cash, org: org, target: :payslip, amount: Money.new(300_00))
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: adjustable_payable,
+        is_auto_adjustable_amount: true
+      )
+
+      attrs = %{
+        due_date: ~D[2021-01-15],
+        reference_date: ~D[2021-01-01],
+        amount: 100_00,
+        description: "Updated description",
+        note: "Updated note",
+        method: :billet,
+        billet_barcode: random_string_number()
+      }
+
+      assert {:ok, %Payable{id: ^id} = return} = Update.call(payslip, payable, attrs)
+
+      assert Repo.get_by(Payable,
+               org_id: return.org_id,
+               id: id,
+               target: payable.target,
+               due_date: attrs[:due_date],
+               reference_date: attrs[:reference_date],
+               amount: attrs[:amount],
+               description: attrs[:description],
+               note: attrs[:note],
+               method: attrs[:method],
+               billet_barcode: attrs[:billet_barcode]
+             )
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: payslip.org_id,
+               payslip_id: payslip.id,
+               payable_id: return.id,
+               is_auto_adjustable_amount: false
+             )
+
+      assert Repo.get_by(Payable, org_id: org.id, id: adjustable_payable.id, amount: 400_00)
+
+      assert Repo.get_by(PayslipPayable,
+               org_id: org.id,
+               payslip_id: payslip.id,
+               payable_id: adjustable_payable.id,
+               is_auto_adjustable_amount: true
              )
     end
 
@@ -69,12 +228,17 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.UpdateTest do
         insert(:payable_cash,
           org: org,
           target: :payslip,
-          amount: 100_00,
+          amount: Money.new(100_00),
           is_fulfilled: true,
           authorized_by_id: user.id
         )
 
-      insert(:payslip_payable, org: org, payslip: payslip, payable: payable, is_auto_adjustable_amount: false)
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: payable,
+        is_auto_adjustable_amount: false
+      )
 
       attrs = %{
         due_date: ~D[2021-01-15],
@@ -107,11 +271,25 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.UpdateTest do
       # Update payslip amount
       Repo.update!(change(payslip, amount: 200_00))
 
-      non_adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 100_00)
-      insert(:payslip_payable, org: org, payslip: payslip, payable: non_adjustable_payable, is_auto_adjustable_amount: false)
+      non_adjustable_payable =
+        insert(:payable_cash, org: org, target: :payslip, amount: Money.new(100_00))
 
-      %{id: id} = payable = insert(:payable_cash, org: org, target: :payslip, amount: 100_00)
-      insert(:payslip_payable, org: org, payslip: payslip, payable: payable, is_auto_adjustable_amount: false)
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: non_adjustable_payable,
+        is_auto_adjustable_amount: false
+      )
+
+      %{id: id} =
+        payable = insert(:payable_cash, org: org, target: :payslip, amount: Money.new(100_00))
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: payable,
+        is_auto_adjustable_amount: false
+      )
 
       attrs = %{
         due_date: ~D[2021-01-15],
@@ -123,12 +301,11 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.UpdateTest do
         billet_barcode: random_string_number()
       }
 
-
       assert {:error, changeset} = Update.call(payslip, payable, attrs)
 
       assert errors_on(changeset) == %{
-        amount: ["can't exceed payslip amount"]
-      }
+               amount: ["can't exceed payslip amount"]
+             }
 
       refute Repo.get_by(Payable,
                org_id: org.id,
@@ -170,14 +347,35 @@ defmodule Sig.Finance.Payables.PayablesForPayslip.UpdateTest do
       # Update payslip amount
       Repo.update!(change(payslip, amount: 300_00))
 
-      non_adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 100_00)
-      insert(:payslip_payable, org: org, payslip: payslip, payable: non_adjustable_payable, is_auto_adjustable_amount: false)
+      non_adjustable_payable =
+        insert(:payable_cash, org: org, target: :payslip, amount: Money.new(100_00))
 
-      auto_adjustable_payable = insert(:payable_cash, org: org, target: :payslip, amount: 100_00)
-      insert(:payslip_payable, org: org, payslip: payslip, payable: auto_adjustable_payable, is_auto_adjustable_amount: true)
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: non_adjustable_payable,
+        is_auto_adjustable_amount: false
+      )
 
-      %{id: id} = payable = insert(:payable_cash, org: org, target: :payslip, amount: 100_00)
-      insert(:payslip_payable, org: org, payslip: payslip, payable: payable, is_auto_adjustable_amount: false)
+      auto_adjustable_payable =
+        insert(:payable_cash, org: org, target: :payslip, amount: Money.new(100_00))
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: auto_adjustable_payable,
+        is_auto_adjustable_amount: true
+      )
+
+      %{id: id} =
+        payable = insert(:payable_cash, org: org, target: :payslip, amount: Money.new(100_00))
+
+      insert(:payslip_payable,
+        org: org,
+        payslip: payslip,
+        payable: payable,
+        is_auto_adjustable_amount: false
+      )
 
       attrs = %{
         due_date: ~D[2021-01-15],
