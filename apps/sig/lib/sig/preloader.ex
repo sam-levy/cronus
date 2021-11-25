794203cba046f29bbf69e242c9f30d7a51ccd605
Samuel Levy <samulevy@gmail.com>
2021-11-25 11:20:39 -0300

    Improve Sig.Preloader

--- apps/sig/lib/sig/preloader.ex
+++ apps/sig/lib/sig/preloader.ex
@@ -2,6 +2,7 @@ defmodule Sig.Preloader do
   defmacro __using__([{schema, preloadable_fields}]) do
     quote bind_quoted: [schema: schema, preloadable_fields: preloadable_fields] do
       import Ecto.Query
+      import Sig.Preloader
 
       for field_name <- preloadable_fields do
         def shallow_preload(queryable, unquote(field_name)) do
@@ -11,25 +12,19 @@ defmodule Sig.Preloader do
           )
           |> preload([{unquote(field_name), field}], [{unquote(field_name), field}])
         end
+      end
 
-        def shallow_preload(queryable, []), do: queryable
+      def shallow_preload(queryable, []), do: queryable
 
-        def shallow_preload(queryable, [{key, _} | _] = opts) when is_atom(key) do
-          case Keyword.get(opts, :preload, []) do
-            [] ->
-              queryable
-
-            fields when is_list(fields) ->
-              Enum.reduce(fields, queryable, &shallow_preload(&2, &1))
-
-            field when is_atom(field) ->
-              shallow_preload(queryable, field)
-          end
+      def shallow_preload(queryable, [{key, _} | _] = opts) when is_atom(key) do
+        case Keyword.get(opts, :preload, []) do
+          [] -> queryable
+          field_or_fields -> shallow_preload(queryable, field_or_fields)
         end
+      end
 
-        def shallow_preload(queryable, fields) when is_list(fields) do
-          Enum.reduce(fields, queryable, &shallow_preload(&2, &1))
-        end
+      def shallow_preload(queryable, fields) when is_list(fields) do
+        Enum.reduce(fields, queryable, &shallow_preload(&2, &1))
       end
     end
   end
