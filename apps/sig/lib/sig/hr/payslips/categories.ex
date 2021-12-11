65e802e0f2c95caf300bfd64f832a480c7676405
Samuel Levy <samulevy@gmail.com>
2021-12-11 12:17:21 -0300

    Add new Categories.list/2 and Categories.get/2 clouses

--- apps/sig/lib/sig/hr/payslips/categories.ex
+++ apps/sig/lib/sig/hr/payslips/categories.ex
@@ -38,11 +38,16 @@ defmodule Sig.HR.Payslips.Categories do
     end
   end
 
+  def list(%Org{} = org), do: list(org.id)
+
   def list(org_id) when is_binary(org_id) do
     Category
     |> where(org_id: ^org_id)
+    |> order_by(:code)
     |> Repo.all()
   end
+
+  def get(%Org{} = org, id) when is_binary(id), do: get(org.id, id)
 
   def get(org_id, id) when is_binary(org_id) and is_binary(id) do
     Category
