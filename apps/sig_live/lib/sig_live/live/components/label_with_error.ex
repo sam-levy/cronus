6b3927eb8c38662da12231ce898116074860092f
Samuel Levy <samulevy@gmail.com>
2022-01-09 20:40:37 -0300

    Add `SigLive.Components.LabelWithError`

---
+++ apps/sig_live/lib/sig_live/live/components/label_with_error.ex
@@ -0,0 +1,16 @@
+defmodule SigLive.Components.LabelWithError do
+  use SigLive, :surface_component
+
+  alias Surface.Components.Form.{ErrorTag, Label}
+
+  slot default
+
+  def render(assigns) do
+    ~F"""
+    <div class="flex">
+      <Label class="form-label"><#slot/></Label>
+      <ErrorTag class="ml-2 form-error-tag"/>
+    </div>
+    """
+  end
+end
