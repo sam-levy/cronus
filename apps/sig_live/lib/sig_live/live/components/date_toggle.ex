3eab02caad2bb5ecf2e2ec5587f9f7c9e2701f32
Samuel Levy <samulevy@gmail.com>
2022-01-09 20:39:03 -0300

    Change `Components.DateToggle` background on hover

--- apps/sig_live/lib/sig_live/live/components/date_toggle.ex
+++ apps/sig_live/lib/sig_live/live/components/date_toggle.ex
@@ -15,7 +15,7 @@
     def render(assigns) do
       ~F"""
       <div class="flex items-center text-gray-500 font-medium text-sm tracking-wider rounded-md">
-        <button :on-click={@previous}>
+        <button :on-click={@previous} class="p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer text-gray-500">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
           </svg>
@@ -31,7 +31,7 @@
           </Field>
         </Form>
 
-        <button :on-click={@next}>
+        <button :on-click={@next} class="p-2 hover:bg-gray-300 hover:rounded-md cursor-pointer text-gray-500">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd" />
             <path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd" />
