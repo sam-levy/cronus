0f2fa44773203c31f6dff6d7f9def12172b9840f
Samuel Levy <samulevy@gmail.com>
2022-01-19 09:53:00 -0300

    Add PG stored procedure to salaries table (#64)
    * Extract and refactor PG stored procedure
    
    * Apply PG stored procedure to `employe_salaries` table

--- apps/sig/priv/repo/migrations/20211007212330_create_employee_company_assignments_table.exs
+++ apps/sig/priv/repo/migrations/20211007212330_create_employee_company_assignments_table.exs
@@ -1,7 +1,7 @@
 defmodule Sig.Repo.Migrations.CreateEmployeeCompanyAssignmentsTable do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table(:employee_company_assignments) do
       add :org_id, references(:orgs), primary_key: true
 
@@ -19,5 +19,28 @@ defmodule Sig.Repo.Migrations.CreateEmployeeCompanyAssignmentsTable do
              [:start_date, :assigned_company_id, :registration_id, :org_id],
              name: :employee_company_assignments_company_start_date
            )
+
+    execute("""
+      CREATE TRIGGER ensure_at_least_one_employee_company_assignment_for_registration_on_delete
+      AFTER DELETE ON employee_company_assignments
+      FOR EACH ROW
+      EXECUTE PROCEDURE ensure_at_least_one_resource_for_registration ();
+    """)
+  end
+
+  def down do
+    execute("""
+      DROP TRIGGER ensure_at_least_one_employee_company_assignment_for_registration_on_delete
+      ON employee_company_assignments;
+    """)
+
+    drop index(:employee_company_assignments, [
+           :start_date,
+           :assigned_company_id,
+           :registration_id,
+           :org_id
+         ])
+
+    drop table(:employee_company_assignments)
   end
 end
