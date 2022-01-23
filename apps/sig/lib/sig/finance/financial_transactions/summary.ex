9f5af555594189d5b49bc6b2600e7df5d6d299ba
Samuel Levy <samulevy@gmail.com>
2022-01-22 22:15:45 -0300

    Add missing `org_id` to `list_for_bank_summary/2`

--- apps/sig/lib/sig/finance/financial_transactions/summary.ex
+++ apps/sig/lib/sig/finance/financial_transactions/summary.ex
@@ -75,7 +75,8 @@ defmodule Sig.Finance.FinancialTransactions.Summary do
       :inner_lateral,
       [payslips: payslip, registrations: registration],
       ca in fragment(
-        "SELECT * FROM employee_company_assignments AS ca WHERE ca.registration_id = ? AND ca.start_date <= ? LIMIT 1",
+        "SELECT * FROM employee_company_assignments AS ca WHERE ca.org_id = ? AND ca.registration_id = ? AND ca.start_date <= ? LIMIT 1",
+        registration.org_id,
         registration.id,
         payslip.start_date
       ),
