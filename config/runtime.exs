2645dad1728673183b5437bbb275a4a43c8353f1
Samuel Levy <samulevy@gmail.com>
2022-01-26 15:17:25 -0300

    3 - Configure `config/runtime.exs` for Fly.io

--- config/runtime.exs
+++ config/runtime.exs
@@ -18,6 +18,7 @@ if config_env() == :prod do
     # ssl: true,
     # socket_options: [:inet6],
     url: database_url,
+    socket_options: [:inet6],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   secret_key_base =
@@ -27,7 +28,12 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
   config :sig_live, SigLive.Endpoint,
+    url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
