8d760fe34ee41ec7e485b2f26413df7d6ced5a1b
Samuel Levy <samulevy@gmail.com>
2022-02-13 15:44:45 -0300

    Add unique constraint to date

--- apps/sig/lib/sig/hr/registrations/overtimes/overtime.ex
+++ apps/sig/lib/sig/hr/registrations/overtimes/overtime.ex
@@ -25,6 +25,7 @@ defmodule Sig.HR.Registrations.Overtimes.Overtime do
     |> validate_required(@create_required_fields)
     |> Sig.Hour.Changeset.validate_hours(:hours_amount)
     |> validate_beginning_of_month(:date)
+    |> unique_constraint([:date, :registration_id, :org_id], name: :employee_overtimes_date_registration_unique)
   end
 
   def update_changeset(%__MODULE__{} = target, attrs) do
@@ -33,6 +34,7 @@ defmodule Sig.HR.Registrations.Overtimes.Overtime do
     |> validate_required([:date, :hours_amount])
     |> Sig.Hour.Changeset.validate_hours(:hours_amount)
     |> validate_beginning_of_month(:date)
+    |> unique_constraint([:date, :registration_id, :org_id], name: :employee_overtimes_date_registration_unique)
   end
 
   def assign_payslip_changeset(%__MODULE__{} = target, attrs) do
