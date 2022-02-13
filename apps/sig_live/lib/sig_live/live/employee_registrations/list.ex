68e59e8657b8d697a238571299826d8f12cef657
Samuel Levy <samulevy@gmail.com>
2022-02-13 14:21:12 -0300

    Add side nav to `SigLive.EmployeeRegistrations.Show`

--- apps/sig_live/lib/sig_live/live/employee_registrations/list.ex
+++ apps/sig_live/lib/sig_live/live/employee_registrations/list.ex
@@ -69,7 +69,7 @@ defmodule SigLive.EmployeeRegistrations.List do
             <tr class="border-b border-gray-200 hover:bg-gray-50">
               <td class="py-3 pl-6 text-left">
                 <LiveRedirect
-                  to={Routes.sig_employee_registrations_show_path(@socket, :registration_show, @org, registration)}
+                  to={Routes.sig_employee_registrations_show_path(@socket, :registration_summary, @org, registration)}
                   class="hover:underline"
                 >
                   <span>{registration.registered_at.trade_name}</span>
