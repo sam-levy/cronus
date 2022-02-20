d096b21ff754c741e04c2302ffa203e9397f8e8c
Samuel Levy <samulevy@gmail.com>
2022-02-20 18:36:29 -0300

    Refactor `SalaryFactory`

--- apps/sig/test/support/factories/salary_factory.ex
+++ apps/sig/test/support/factories/salary_factory.ex
@@ -6,16 +6,17 @@ defmodule Sig.Factories.SalaryFactory do
       def factory(:employee_salary, attrs) do
         org = Keyword.get(attrs, :org) || insert(:org)
 
-        admission_date = random_past_date() |> Date.beginning_of_month()
+        start_date =
+          Keyword.get(attrs, :start_date) || random_past_date() |> Date.beginning_of_month()
 
         registration =
           Keyword.get(attrs, :registration) ||
-            insert(:employee_registration, org: org, admission_date: admission_date)
+            insert(:employee_registration, org: org, admission_date: start_date)
 
         %Salary{
           org: org,
           registration: registration,
-          start_date: admission_date,
+          start_date: start_date,
           amount: Enum.random(1_200_00..4_000_00)
         }
       end
