a5ebe874a6ae2e1755a696e32ae2892e7d5d2ca8
Samuel Levy <samulevy@gmail.com>
2022-02-20 15:20:13 -0300

    Refactor `SigLive.EmployeeRegistrations.Summary`

--- apps/sig_live/lib/sig_live/live/employee_registrations/summary.ex
+++ apps/sig_live/lib/sig_live/live/employee_registrations/summary.ex
@@ -3,12 +3,18 @@ defmodule SigLive.EmployeeRegistrations.Summary do
 
   alias SigLive.Components.DropdownBtn
   alias SigLive.EmployeeRegistrations.Form
+  alias SigLive.EmployeeRegistrations.ResignationForm
+
+  alias Sig.HR
+
+  @broadcast_opts [preload: [:registered_at, :sector, :position]]
 
   prop registration, :struct, required: true
   prop individual, :struct, required: true
   prop org, :struct, required: true
 
   data form_state, :atom, default: :closed, values!: Form.states()
+  data resignation_form_open, :boolean, default: false
 
   @impl true
   def handle_event("open_form", _, socket) do
@@ -16,8 +22,35 @@ defmodule SigLive.EmployeeRegistrations.Summary do
   end
 
   @impl true
-  def handle_event("close_form", _, socket) do
+  def handle_event("open_registration_resignation_form", _, socket) do
+    {:noreply, assign(socket, resignation_form_open: true)}
+  end
+
+  @impl true
+  def handle_event("close_modals", _, socket) do
     {:noreply, assign(socket, closed_state())}
+  end
+
+  @impl true
+  def handle_event("undo_resignation", _params, socket) do
+    %{registration: registration, individual: individual} = socket.assigns
+
+    with {:ok, registration} <- HR.undo_registration_resignation(registration) do
+      HR.broadcast_updated_individual_registration(individual, registration, @broadcast_opts)
+      flash_info("Desligamento desfeito")
+
+      {:noreply, socket}
+    else
+      {:error, changeset} when is_struct(changeset) ->
+        flash_error(Sig.Changeset.errors_to_string(changeset))
+
+        {:noreply, socket}
+
+      {:error, message} when is_binary(message) ->
+        flash_error(message)
+
+        {:noreply, socket}
+    end
   end
 
   @impl true
@@ -27,12 +60,21 @@ defmodule SigLive.EmployeeRegistrations.Summary do
       <Form
         :if={@form_state != :closed}
         id="registration_form"
-        close_event="close_form"
-        close_fun={fn -> close_form(@id) end}
+        close_event="close_modals"
+        close_fun={fn -> close_modals(@id) end}
         registration_id={@registration.id}
         {=@form_state}
         {=@individual}
         {=@org}
+      />
+
+      <ResignationForm
+        :if={@resignation_form_open}
+        id="registration_resignation_form"
+        close_event="close_modals"
+        close_fun={fn -> close_modals(@id) end}
+        registration_id={@registration.id}
+        {=@individual}
       />
 
       <div class="bg-white shadow-lg overflow-hidden">
@@ -43,6 +85,12 @@ defmodule SigLive.EmployeeRegistrations.Summary do
 
           <DropdownBtn>
             <a :on-click="open_form" class="dropdown-item">Editar</a>
+
+            {#if is_nil(@registration.resignation_date)}
+              <a :on-click="open_registration_resignation_form" class="dropdown-item">Desligar Funcionário</a>
+            {#else}
+              <a :on-click="undo_resignation" class="dropdown-item">Desfazer Desligamento</a>
+            {/if}
           </DropdownBtn>
         </div>
 
@@ -50,43 +98,45 @@ defmodule SigLive.EmployeeRegistrations.Summary do
           <dl class="divide-y divide-gray-200">
             <div class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
               <dt class="text-sm font-medium text-gray-500">Empresa</dt>
-              <dd class="mt-1 text-sm text-gray-500">{@registration.registered_at.registration_name}</dd>
+              <dd class="text-sm text-gray-500">{@registration.registered_at.registration_name}</dd>
             </div>
 
             <div class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
               <dt class="text-sm font-medium text-gray-500">Data de admissão</dt>
-              <dd class="mt-1 text-sm text-gray-500">{format_date(@registration.admission_date)}</dd>
+              <dd class="text-sm text-gray-500">{format_date(@registration.admission_date)}</dd>
             </div>
 
             <div class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
               <dt class="text-sm font-medium text-gray-500">Número do registro</dt>
-              <dd class="mt-1 text-sm text-gray-500">{@registration.number}</dd>
+              <dd class="text-sm text-gray-500">{@registration.number}</dd>
             </div>
 
             <div class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
               <dt class="text-sm font-medium text-gray-500">Matrícula eSocial</dt>
-              <dd class="mt-1 text-sm text-gray-500">{@registration.e_social_number}</dd>
+              <dd class="text-sm text-gray-500">{@registration.e_social_number}</dd>
             </div>
 
             <div class="px-4 py-2  grid grid-cols-2 hover:bg-gray-50">
               <dt class="text-sm font-medium text-gray-500">Setor</dt>
-              <dd class="mt-1 text-sm text-gray-500">{@registration.sector.name}</dd>
+              <dd class="text-sm text-gray-500">{@registration.sector.name}</dd>
             </div>
 
             <div class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
               <dt class="text-sm font-medium text-gray-500">Cargo</dt>
-              <dd class="mt-1 text-sm text-gray-500">{@registration.position.name}</dd>
+              <dd class="text-sm text-gray-500">{@registration.position.name}</dd>
             </div>
 
-            <div :if={@registration.resignation_date} class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
-              <dt class="text-sm font-medium text-gray-500">Data de desligamento</dt>
-              <dd class="mt-1 text-sm text-gray-500">{@registration.resignation_date}</dd>
-            </div>
+            {#if @registration.resignation_date}
+              <div class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
+                <dt class="text-sm font-medium text-gray-500">Data de desligamento</dt>
+                <dd class="text-sm text-gray-500">{format_date(@registration.resignation_date)}</dd>
+              </div>
 
-            <div :if={@registration.resignation_date} class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
-              <dt class="text-sm font-medium text-gray-500">Tipo de desligamento</dt>
-              <dd class="mt-1 text-sm text-gray-500">{@registration.resignation_type}</dd>
-            </div>
+              <div class="px-4 py-2 grid grid-cols-2 hover:bg-gray-50">
+                <dt class="text-sm font-medium text-gray-500">Motivo do desligamento</dt>
+                <dd class="text-sm text-gray-500">{capitalize_type(@registration.resignation_type)}</dd>
+              </div>
+            {/if}
           </dl>
         </div>
       </div>
@@ -94,8 +144,8 @@ defmodule SigLive.EmployeeRegistrations.Summary do
     """
   end
 
-  def close_form(id), do: send_update(__MODULE__, closed_state(id))
+  def close_modals(id), do: send_update(__MODULE__, closed_state(id))
 
-  defp closed_state, do: [form_state: :closed, registration_id: nil]
+  defp closed_state, do: [form_state: :closed, resignation_form_open: false, registration_id: nil]
   defp closed_state(id), do: closed_state() ++ [id: id]
 end
