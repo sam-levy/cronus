ddcc0d6dce9216a50b0a81a299116068770a00b2
Samuel Levy <samulevy@gmail.com>
2022-03-01 18:23:28 -0300

    Add `employeeCountPerDesignatedCompany` chart

---
+++ apps/sig_live/assets/js/charts.js
@@ -0,0 +1,48 @@
+import Chart from 'chart.js/auto'
+
+export const employeeCountPerDesignatedCompany = {
+  mounted() {
+    const ctx = this.el.getContext('2d');
+
+    const config = {
+      type: 'pie',
+      data: {
+        labels: [],
+        datasets: [{
+          backgroundColor: [],
+          data: []
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: 'right',
+            labels: {
+              generateLabels: chart => {
+                const datasets = chart.data.datasets;
+
+                return datasets[0].data.map((data, i) => ({
+                  text: `${chart.data.labels[i]} - ${data}`,
+                  fillStyle: datasets[0].backgroundColor[i],
+                }))
+              }
+            }
+          }
+        }
+      }
+    }
+
+    const chart = new Chart(ctx, config);
+
+    this.handleEvent("employee_count_per_designated_company", ({ data }) => {
+      const values = Object.values(data)
+
+      chart.data.labels = Object.keys(data)
+      chart.data.datasets[0].data = values.map(value => value.count)
+      chart.data.datasets[0].backgroundColor = values.map(value => value.color)
+
+      chart.update()
+    })
+  }
+}
--- apps/sig_live/lib/sig_live/live/reports/hr.ex
+++ apps/sig_live/lib/sig_live/live/reports/hr.ex
@@ -1,13 +1,32 @@
 defmodule SigLive.Reports.HR do
   use SigLive, :surface_live_view
 
+  alias Sig.Reports
+  alias Sig.Entities
+
   alias SigLive.Components.AppMenu
 
   @impl true
   def mount(_params, _session, socket) do
     %{org: org} = socket.assigns
 
-    {:ok, socket}
+    {:ok, push_employee_count_per_designated_company(socket, org)}
+  end
+
+  defp push_employee_count_per_designated_company(socket, org) do
+    data =
+      org
+      |> Reports.employee_count_per_designated_company()
+      |> put_company_colors()
+
+    push_event(socket, "employee_count_per_designated_company", %{data: data})
+  end
+
+  defp put_company_colors(data) do
+    Map.new(data, fn {company_name, data} ->
+      {company_name,
+       Map.put(data, :color, Entities.get_company_color_by_trade_name(company_name))}
+    end)
   end
 
   @impl true
@@ -24,6 +43,23 @@ defmodule SigLive.Reports.HR do
         <AppMenu.Breadcrumb name="RH" />
       </AppMenu>
 
+      <div class="w-full bg-white shadow-lg">
+        <div class="py-3 px-6">
+          <span class="text-gray-500 font-medium tracking-wider">
+            Funcionários Por Empresa
+          </span>
+        </div>
+
+        <div class="border-t border-gray-200 flex justify-center">
+          <div class="w-96 p-3">
+            <canvas
+              id="employee_count_per_designated_company"
+              phx-hook="employeeCountPerDesignatedCompany"
+              phx-update="ignore"
+            />
+          </div>
+        </div>
+      </div>
     </div>
     """
   end
