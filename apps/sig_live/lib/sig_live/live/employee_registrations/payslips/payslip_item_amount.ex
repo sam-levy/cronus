594fa3b8f02a2ca2681536fccd948f42d5240921
Samuel Levy <samulevy@gmail.com>
2022-03-02 20:54:18 -0300

    Add `PayslipItemAmount` component

---
+++ apps/sig_live/lib/sig_live/live/employee_registrations/payslips/payslip_item_amount.ex
@@ -0,0 +1,25 @@
+defmodule SigLive.EmployeeRegistrations.Payslips.PayslipItemAmount do
+  use SigLive, :surface_component
+
+  prop item, :struct, required: true
+  prop edit_event, :event, required: true
+  prop is_editable, :boolean, required: false
+
+  def render(assigns) do
+    ~F"""
+    {#if @is_editable}
+      <a
+        :on-click={@edit_event}
+        phx-value-item_id={@item.id}
+        class="p-1.5 rounded hover:bg-gray-200 border border-transparent hover:border-gray-400"
+      >
+        {@item.amount}
+      </a>
+    {#else}
+      <span class="p-1.5 border border-transparent">
+        {@item.amount}
+      </span>
+    {/if}
+    """
+  end
+end
