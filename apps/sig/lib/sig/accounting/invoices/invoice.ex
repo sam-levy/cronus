2e747a81cb8f4cc97f47d36e0f0b7723ac3c3af6
Samuel Levy <samulevy@gmail.com>
2023-02-05 16:08:21 -0300

    Add and test missing unique constratint

--- apps/sig/lib/sig/accounting/invoices/invoice.ex
+++ apps/sig/lib/sig/accounting/invoices/invoice.ex
@@ -64,6 +64,8 @@ defmodule Sig.Accounting.Invoices.Invoice do
     |> validate_numericality(:number)
     |> validate_length(:number, max: 9)
     |> validate_money(:amount, [:gt, :eq], 0)
-    |> unique_constraint([:number, :invoiced_by_id, :org_id])
+    |> unique_constraint([:number, :invoiced_by_id, :org_id],
+      name: :invoices_number_invoiced_by_id_unique
+    )
   end
 end
